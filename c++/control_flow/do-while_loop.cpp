0e2babbcd62072b08e6bfeb78546afdf94e876b6
katheroine <katheroine@gmail.com>
2021-07-27 11:14:43 +0200

    C++ do-while loop [#control_flow]

---
+++ c++/control_flow/do-while_loop.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+  int i = 0;
+
+  do
+  {
+    std::cout << i << "...\n";
+    ++i;
+  } while (i < 10);
+
+  std::cout << std::endl;
+
+  i = 0;
+
+  do
+  {
+    std::cout << i++ << "...\n";
+  } while (i < 10);
+
+  std::cout << std::endl;
+
+  i = 0;
+
+  do
+    std::cout << i++ << "...\n";
+  while (i < 10);
+
+  std::cout << std::endl;
+
+  i = 0;
+
+  do std::cout << i++ << "...\n"; while (i < 10);
+}
