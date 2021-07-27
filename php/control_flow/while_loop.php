ffd178a5083ee35c497bf2791a47b8e2f61079a6
katheroine <katheroine@gmail.com>
2021-07-27 09:59:43 +0200

    PHP while loop [#control_flow]

---
+++ php/control_flow/while_loop.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+
+$number = 0;
+
+while ($number < 10) {
+  echo("{$number}...\n");
+  ++$number;
+}
+
+echo "\n";
+
+$number = 0;
+
+// Shortened form for HTML templates:
+while ($number < 10):
+  echo("{$number}...\n");
+  ++$number;
+endwhile;
+
+echo "\n";
+
+$number = 0;
+
+while ($number < 10) { echo("{$number}...\n"); ++$number; }
+
+echo "\n";
+
+$number = 0;
+
+// Shortened form for HTML templates:
+while ($number < 10): echo("{$number}...\n"); ++$number; endwhile;
