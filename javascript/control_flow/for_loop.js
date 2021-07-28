8c9e1c79841bcad20a40e02f5bd7a1ca3a1ea3b8
katheroine <katheroine@gmail.com>
2021-07-28 09:54:17 +0200

    JavaScript for loop [#control_flow]

---
+++ javascript/control_flow/for_loop.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+for(let i = 0; i < 10; i++)
+  console.log(`${i}...`);
+
+console.log();
+
+for(let i = 0; i < 10; i++) console.log(`${i}...`);
+
+console.log();
+
+for(let i = 0; i < 10; i++) {
+  console.log(`${i}...`);
+}
