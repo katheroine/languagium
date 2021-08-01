78a2486951ea273d77638252996815f5c5aef8cd
katheroine <katheroine@gmail.com>
2021-08-01 20:06:01 +0200

    JavaScript switch statement [#control_flow]

---
+++ javascript/control_flow/switch_statement.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+let now = "afternoon";
+
+switch (now)
+{
+  case "morning":
+    console.log("Good morning!");
+    break;
+  case "afternoon":
+    console.log("Good afternoon!");
+    break;
+  case "evening":
+    console.log("Good evening!");
+    break;
+  case "night":
+    console.log("Good evening!");
+    break;
+}
+
+now = "evening";
+
+switch (now)
+{
+  case "morning":
+    console.log("Good morning!");
+    break;
+  case "afternoon":
+    console.log("Good afternoon!");
+    break;
+  case "evening":
+  case "night":
+    console.log("Good evening!");
+    break;
+}
+
+now = "other";
+
+switch (now)
+{
+  case "morning":
+    console.log("Good morning!");
+    break;
+  case "afternoon":
+    console.log("Good afternoon!");
+    break;
+  case "evening":
+    console.log("Good evening!");
+    break;
+  case "night":
+    console.log("Good evening!");
+    break;
+  default:
+    console.log("Hello!");
+    break;
+}
