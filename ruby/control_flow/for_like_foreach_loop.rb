f4b6cc31f13d59fa645f305f718004edccff9495
katheroine <katheroine@gmail.com>
2021-08-01 12:02:13 +0200

    Ruby foreach-like for loop [#control_flow]

---
+++ ruby/control_flow/for_like_foreach_loop.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+a = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+for i in a
+  puts "#{i}..."
+end
+
+puts
+
+for i in a do puts "#{i}..." end
+
+puts
+
+for i in a; puts "#{i}..." end
