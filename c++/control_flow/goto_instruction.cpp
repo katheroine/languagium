66f40a68bb7b0cf420df80351889ca9bff1fcf4d
katheroine <katheroine@gmail.com>
2021-08-02 11:16:37 +0200

    C++ goto instruction [#control_flow]

---
+++ c++/control_flow/goto_instruction.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+  int c, a;
+
+  c = 10;
+  a = 0;
+
+begin:
+  a += c;
+  c--;
+  if (c == 0)
+    goto end;
+  goto begin;
+
+end:
+  std::cout << a << std::endl;
+}
