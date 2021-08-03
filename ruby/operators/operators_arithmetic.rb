e14fcce3a64564a164176fece8786d9f6e9bf48f
katheroine <katheroine@gmail.com>
2021-08-03 15:22:36 +0200

    Ruby arithmetic operators [#operators]

---
+++ ruby/operators/operators_arithmetic.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+a = 6; b = 2
+
+c = a + b;
+puts "#{a} + #{b} = #{c}"
+c = a - b;
+puts "#{a} - #{b} = #{c}"
+c = a * b;
+puts "#{a} * #{b} = #{c}"
+c = a / b;
+puts "#{a} / #{b} = #{c}"
+c = a % b;
+puts "#{a} % #{b} = #{c}"
+
+puts "-#{a} = #{-a}"
+puts "+#{b} = #{+b}"
