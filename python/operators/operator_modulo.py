2b81a96b25419290663d13603cd512680b04b50a
katheroine <katheroine@gmail.com>
2021-08-03 13:29:14 +0200

    Python arithmetic operator modulo [#operators]

---
+++ python/operators/operator_modulo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+i1 = 6; i2 = 1
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+i1 = 6; i2 = 2
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+i1 = 6; i2 = 3
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+i1 = 6; i2 = 4
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+i1 = 6; i2 = 5
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+i1 = 6; i2 = 6
+print("{} % {} = {}".format(i1, i2, (i1 % i2)))
+
+f1 = 6.4; f2 = 3.2
+print("{} % {} = {}".format(f1, f2, (f1 % f2)))
