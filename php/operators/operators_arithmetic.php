459c6c06ce5c8db9c8e5df51897ba69062837498
katheroine <katheroine@gmail.com>
2021-08-03 12:42:08 +0200

    PHP arithmetic operators [#operators]

---
+++ php/operators/operators_arithmetic.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+
+$a = 6; $b = 2;
+
+$c = $a + $b;
+print("{$a} + {$b} = {$c}\n");
+$c = $a - $b;
+print("{$a} - {$b} = {$c}\n");
+$c = $a * $b;
+print("{$a} * {$b} = {$c}\n");
+$c = $a / $b;
+print("{$a} / {$b} = {$c}\n");
+$c = $a % $b;
+print("{$a} % {$b} = {$c}\n");
+
+print("-{$a} = " . -$a . "\n");
+print("+{$b} = " . +$b . "\n");
