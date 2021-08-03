fac43e383349c794009c7727409b646465674968
katheroine <katheroine@gmail.com>
2021-08-03 17:23:20 +0200

    JavaScript arithmetic operators [#operators]

---
+++ javascript/operators/operators_arithmetic.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+let a = 6, b = 2, c = 0;
+
+c = a + b;
+console.log(`${a} + ${b} = ${c}`);
+c = a - b;
+console.log(`${a} - ${b} = ${c}`);
+c = a * b;
+console.log(`${a} * ${b} = ${c}`);
+c = a / b;
+console.log(`${a} / ${b} = ${c}`);
+c = a % b;
+console.log(`${a} % ${b} = ${c}`);
+
+console.log(`-${a} = ${-a}`);
+console.log(`+${b} = ${+b}`);
