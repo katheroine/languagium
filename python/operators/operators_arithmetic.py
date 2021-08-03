7ef21c76b0d42388482b24bba692c0b2457789fb
katheroine <katheroine@gmail.com>
2021-08-03 13:28:51 +0200

    Python arithmetic operators [#operators]

---
+++ python/operators/operators_arithmetic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+a = 6; b = 2
+
+c = a + b
+print("{} + {} = {}".format(a, b, c))
+c = a - b
+print("{} - {} = {}".format(a, b, c))
+c = a * b
+print("{} * {} = {}".format(a, b, c))
+c = a / b
+print("{} / {} = {}".format(a, b, c))
+c = a % b
+print("{} % {} = {}".format(a, b, c))
+
+print("-{} = {}".format(a, -a))
+print("+{} = {}".format(b, +b))
