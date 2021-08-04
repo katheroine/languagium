18eff2bf6c54403ef1af95ccaac4bfc7ab9439d2
katheroine <katheroine@gmail.com>
2021-08-04 18:15:34 +0200

    JavaScript arithmetic operator modulo [#operators]

---
+++ javascript/operators/operator_modulo.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+let i1 = 6, i2 = 1;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+i1 = 6, i2 = 2;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+i1 = 6, i2 = 3;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+i1 = 6, i2 = 4;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+i1 = 6, i2 = 5;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+i1 = 6, i2 = 6;
+console.log(`${i1} % ${i2} = ${i1 % i2}`);
+
+let f1 = 6.4, f2 = 3.2;
+console.log(`${f1} % ${f2} = ${f1 % f2}`);
