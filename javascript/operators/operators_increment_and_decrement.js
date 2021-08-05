e62408067add52f09ec8dcbb657eacd0b728b963
katheroine <katheroine@gmail.com>
2021-08-05 13:17:15 +0200

    JavaScript increment & decrement operators [#operators]

---
+++ javascript/operators/operators_increment_and_decrement.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+let a = 6, b = 3;
+
+console.log(`a = ${a}, b = ${b}`);
+console.log(`++a : ${++a}(${a})`);
+console.log(`--b : ${--b}(${b})`);
+console.log(`a = ${a}, b = ${b}`);
+console.log(`a++ : ${a++}(${a})`);
+console.log(`b-- : ${b--}(${b})`);
