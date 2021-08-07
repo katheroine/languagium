ff0bf95814123359e7123c13ccdf08d4ec2a678e
katheroine <katheroine@gmail.com>
2021-08-07 13:14:36 +0200

    JavaScript exponentiation operator added to arithmetic operators example [#operators]

--- javascript/operators/operators_arithmetic.js
+++ javascript/operators/operators_arithmetic.js
@@ -12,6 +12,8 @@ c = a / b;
 console.log(`${a} / ${b} = ${c}`);
 c = a % b;
 console.log(`${a} % ${b} = ${c}`);
+c = a ** b;
+console.log(`${a} ** ${b} = ${c}`);
 
 console.log(`-${a} = ${-a}`);
 console.log(`+${b} = ${+b}`);
