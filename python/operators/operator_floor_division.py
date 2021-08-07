9bbfe0e5c51f9041b8f1872541e6c44430fa71ae
katheroine <katheroine@gmail.com>
2021-08-07 13:48:48 +0200

    Python floor division arithmetic operator [#operators]

---
+++ python/operators/operator_floor_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+i1 = 6; i2 = 3
+print("{} / {} = {}".format(i1, i2, (i1 / i2)))
+print("{} // {} = {}\n".format(i1, i2, (i1 // i2)))
+i1 = 6; i2 = 4
+print("{} / {} = {}".format(i1, i2, (i1 / i2)))
+print("{} // {} = {}\n".format(i1, i2, (i1 // i2)))
+i1 = 6; i2 = 5
+print("{} / {} = {}".format(i1, i2, (i1 / i2)))
+print("{} // {} = {}\n".format(i1, i2, (i1 // i2)))
+
+f1 = 6.4; f2 = 3.2
+print("{} / {} = {}".format(f1, f2, (f1 / f2)))
+print("{} // {} = {}\n".format(f1, f2, (f1 // f2)))
+f1 = 6.4; f2 = 2.0
+print("{} / {} = {}".format(f1, f2, (f1 / f2)))
+print("{} // {} = {}\n".format(f1, f2, (f1 // f2)))
+f1 = 6.4; f2 = 2.2
+print("{} / {} = {}".format(f1, f2, (f1 / f2)))
+print("{} // {} = {}\n".format(f1, f2, (f1 // f2)))
