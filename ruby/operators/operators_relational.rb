4f856a7043838f60cacc608911e32632960b4cd9
katheroine <katheroine@gmail.com>
2021-08-07 08:39:35 +0200

    Ruby relational operators [#operators]

---
+++ ruby/operators/operators_relational.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+a = 1; b = 2
+
+c = a < b
+puts("#{a} < #{b} = #{c}")
+c = a > b
+puts("#{a} > #{b} = #{c}")
+c = a <= b
+puts("#{a} <= #{b} = #{c}")
+c = a >= b
+puts("#{a} >= #{b} = #{c}")
+c = a == b
+puts("#{a} == #{b} = #{c}")
+c = a != b
+puts("#{a} != #{b} = #{c}")
