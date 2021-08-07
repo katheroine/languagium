313ec0e757098ed0b61a43863b1489ca723a7ebd
katheroine <katheroine@gmail.com>
2021-08-07 08:25:11 +0200

    PHP relational operators [#operators]

---
+++ php/operators/operators_relational.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+$a = 1; $b = 2; $c = false;
+
+$c = $a < $b;
+print("{$a} < {$b} = {$c}\n");
+$c = $a > $b;
+print("{$a} > {$b} = {$c}\n");
+$c = $a <= $b;
+print("{$a} <= {$b} = {$c}\n");
+$c = $a >= $b;
+print("{$a} >= {$b} = {$c}\n");
+$c = $a == $b;
+print("{$a} == {$b} = {$c}\n");
+$c = $a != $b;
+print("{$a} != {$b} = {$c}\n");
