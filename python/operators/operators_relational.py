688b36831e3689bf16f3b6c51656aa2ef4531a97
katheroine <katheroine@gmail.com>
2021-08-07 08:33:07 +0200

    Python relational operators [#operators]

---
+++ python/operators/operators_relational.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+a = 1; b = 2
+
+c = a < b
+print(f"{a} < {b} = {c}")
+c = a > b
+print(f"{a} > {b} = {c}")
+c = a <= b
+print(f"{a} <= {b} = {c}")
+c = a >= b
+print(f"{a} >= {b} = {c}")
+c = a == b
+print(f"{a} == {b} = {c}")
+c = a != b
+print(f"{a} != {b} = {c}")
