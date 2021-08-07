c8700659ef5d5217cb58113411c661519eb0538b
katheroine <katheroine@gmail.com>
2021-08-07 08:40:57 +0200

    JavaScript relational operators [#operators]

---
+++ javascript/operators/operators_relational.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+let a = 1, b = 2, c = false;
+
+c = a < b
+console.log(`${a} < ${b} = ${c}`);
+c = a > b
+console.log(`${a} > ${b} = ${c}`);
+c = a <= b
+console.log(`${a} <= ${b} = ${c}`);
+c = a >= b
+console.log(`${a} >= ${b} = ${c}`);
+c = a == b
+console.log(`${a} == ${b} = ${c}`);
+c = a != b
+console.log(`${a} != ${b} = ${c}`);
