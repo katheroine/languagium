bdbd7fb2d793f81538f51dbe20b401348a4e2f66
katheroine <katheroine@gmail.com>
2021-08-09 07:49:07 +0200

    C++ bitwise operators [#operators]

---
+++ c++/operators/operators_bitwise.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Binary representation in the comments for 32-bit computer
+
+int main()
+{
+  unsigned int a = 10; // 0000 0000 0000 0000 0000 0000 0000 1010
+  unsigned int b = 12; // 0000 0000 0000 0000 0000 0000 0000 1100
+  unsigned int c;
+
+  c = a & b;
+  std::cout << a << " & " << b << " = " << c << std::endl; // 0000 0000 0000 0000 0000 0000 0000 1000 // 8
+  c = a | b;
+  std::cout << a << " | " << b << " = " << c << std::endl; // 0000 0000 0000 0000 0000 0000 0000 1110 // 14
+  c = a ^ b;
+  std::cout << a << " ^ " << b << " = " << c << std::endl; // 0000 0000 0000 0000 0000 0000 0000 0110 // 6
+
+  std::cout << "~" << a << " = " << ~a << std::endl; // 1111 1111 1111 1111 1111 1111 1111 0101 // 4294967285
+  std::cout << "~" << b << " = " << ~b << std::endl; // 1111 1111 1111 1111 1111 1111 1111 0011 // 4294967283
+}
