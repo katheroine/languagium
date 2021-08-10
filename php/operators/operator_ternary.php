0b2accbae43cebeb31ea30b10ce04ae7a710aeaf
katheroine <katheroine@gmail.com>
2021-08-10 13:38:10 +0200

    PHP ternary (conditional) operator [#operators]

---
+++ php/operators/operator_ternary.php
@@ -0,0 +1,10 @@
+#!/usr/bin/php
+<?php
+
+$value = readline("Give some value: ");
+
+$state = ($value < 10) ? "low" : "high";
+
+print("Value is {$state}. ");
+
+($value < 10) ? print("Cool!\n") : print("Woah!\n");
