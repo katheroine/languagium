b15b3fc1060b61ad01bf81e4a5eafef2c023ea9b
katheroine <katheroine@gmail.com>
2021-08-17 12:24:13 +0200

    JavaScript examples of array elements [#arrays]

---
+++ javascript/arrays/examples_of_array_elements.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+let numbers = [];
+numbers[0] = 2;
+numbers[1] = 4;
+numbers[2] = 6;
+
+console.log(`numbers[0]: ${numbers[0]}`);
+console.log(`numbers[1]: ${numbers[1]}`);
+console.log(`numbers[2]: ${numbers[2]}\n`);
+
+let values = [9.5, 8.5, 7.5];
+
+console.log(`values[0]: ${values[0]}`);
+console.log(`values[1]: ${values[1]}`);
+console.log(`values[2]: ${values[2]}\n`);
+
+let words = ["first", "two", "last"];
+
+console.log(`values[0]: ${values[0]}`);
+console.log(`values[1]: ${values[1]}`);
+console.log(`values[2]: ${values[2]}\n`);
+
+let items = [
+  321,
+  2.5,
+  "orange",
+  [2, 4, 6],
+  {"name": "object", "value": 3.5}
+];
+
+console.log(`items[0]: ${items[0]}`);
+console.log(`items[1]: ${items[1]}`);
+console.log(`items[2]: ${items[2]}`);
+console.log(`items[3]: ${items[3]}`);
+console.log(`items[4]: ${items[4]}\n`);
