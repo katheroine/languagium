4da5e33a6fb2a96b0f00345f2415de6b8a6faf6b
katheroine <katheroine@gmail.com>
2021-08-17 11:35:06 +0200

    Ruby examples of array elements [#arrays]

---
+++ ruby/arrays/examples_of_array_elements.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby2.7
+
+numbers = [2, 4, 6]
+
+puts("numbers[0]: #{numbers[0]}")
+puts("numbers[1]: #{numbers[1]}")
+puts("numbers[2]: #{numbers[2]}\n\n")
+
+values = [9.5, 8.5, 7.5]
+
+puts("values[0]: #{values[0]}")
+puts("values[1]: #{values[1]}")
+puts("values[2]: #{values[2]}\n\n")
+
+words = ["first", "two", "last"]
+
+puts("words[0]: #{words[0]}")
+puts("words[1]: #{words[1]}")
+puts("words[2]: #{words[2]}\n\n")
+
+items = [
+  321,
+  2.5,
+  "orange",
+  [2, 4, 6],
+  (10..30)
+]
+
+puts("items[0]: #{items[0]}")
+puts("items[1]: #{items[1]}")
+puts("items[2]: #{items[2]}")
+puts("items[3]: #{items[3]}")
+puts("items[4]: #{items[4]}\n\n")
