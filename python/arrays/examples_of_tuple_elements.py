fb80aa9e325f1e39adcf28d6d42c9f240e987d76
katheroine <katheroine@gmail.com>
2021-08-25 09:03:56 +0200

    Python examples of array elements split into lists and tuples [#arrays]

---
+++ python/arrays/examples_of_tuple_elements.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+numbers = (2, 4, 6)
+
+print(f"numbers[0]: {numbers[0]}")
+print(f"numbers[1]: {numbers[1]}")
+print(f"numbers[2]: {numbers[2]}\n")
+
+values = (9.5, 8.5, 7.5)
+
+print(f"values[0]: {values[0]}")
+print(f"values[1]: {values[1]}")
+print(f"values[2]: {values[2]}\n")
+
+words = ("first", "two", "last")
+
+print(f"words[0]: {words[0]}")
+print(f"words[1]: {words[1]}")
+print(f"words[2]: {words[2]}\n")
+
+items = (
+  321,
+  2.5,
+  "orange",
+  [2, 4, 6],
+  (9.5, 8.5, 7.5),
+  range(10, 30)
+)
+
+print(f"items[0]: {items[0]}")
+print(f"items[1]: {items[1]}")
+print(f"items[2]: {items[2]}")
+print(f"items[3]: {items[3]}")
+print(f"items[4]: {items[4]}")
+print(f"items[5]: {items[5]}\n")
