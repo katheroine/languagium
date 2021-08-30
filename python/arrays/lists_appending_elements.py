cc2160f6e4895d4f94c54860d2a46b8ad6507812
katheroine <katheroine@gmail.com>
2021-08-30 10:13:13 +0200

    Python appending element to an array [#arrays]

---
+++ python/arrays/lists_appending_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from pprint import pprint
+
+list = [2, 4, 6]
+
+print("Initial state:\n")
+pprint(list); print()
+
+list.append(8)
+
+print("After append by list.append(8):\n")
+pprint(list); print()
