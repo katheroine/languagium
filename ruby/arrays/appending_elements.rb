6ce97617cdf4ae6cae85b8ad31133e9714f59af2
katheroine <katheroine@gmail.com>
2021-08-30 10:45:45 +0200

    Ruby appending element to an array [#arrays]

---
+++ ruby/arrays/appending_elements.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby2.7
+
+array = [2, 4, 6]
+
+puts("Initial state:\n\n")
+puts(array.inspect, "\n")
+
+array << 8
+
+puts("After append by array << 8:\n\n")
+puts(array.inspect, "\n")
+
+array.append(10)
+
+puts("After append by array.append(10):\n\n")
+puts(array.inspect, "\n")
+
+array.append(12, 14, 16)
+
+puts("After append by array.append(12, 14, 16):\n\n")
+puts(array.inspect, "\n")
+
+array.push(18)
+
+puts("After append by array.push(18):\n\n")
+puts(array.inspect, "\n")
+
+array.push(20, 22, 24)
+
+puts("After append by array.push(20, 22, 24):\n\n")
+puts(array.inspect, "\n")
