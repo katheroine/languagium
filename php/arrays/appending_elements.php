56b4016c6b4dabff271b0008f0fb1acbe5fd22cf
katheroine <katheroine@gmail.com>
2021-08-30 10:12:36 +0200

    PHP appending element to an array [#arrays]

---
+++ php/arrays/appending_elements.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php8.0
+<?php
+
+$array = [2, 4, 6];
+
+print("Initial state:\n\n");
+print_r($array); print("\n");
+
+$array[] = 8;
+
+print("After append by \$array[] = 8:\n\n");
+print_r($array); print("\n");
+
+array_push($array, 10);
+
+print("After append by array_push(\$array[], 10):\n\n");
+print_r($array); print("\n");
+
+array_push($array, 12, 14, 16);
+
+print("After append by array_push(\$array[], 12, 14, 16):\n\n");
+print_r($array); print("\n");
