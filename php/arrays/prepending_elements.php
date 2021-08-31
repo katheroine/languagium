e454ab3cfb9d1ecbf5fcb5c2c6d20b040e322097
katheroine <katheroine@gmail.com>
2021-08-31 10:48:14 +0200

    PHP prepending element to an array [#arrays]

---
+++ php/arrays/prepending_elements.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php8.0
+<?php
+
+$array = [2, 4, 6];
+
+print("Initial state:\n\n");
+print_r($array); print("\n");
+
+array_unshift($array, 10);
+
+print("After append by array_unshift(\$array, 0):\n\n");
+print_r($array); print("\n");
+
+array_unshift($array, -6, -4, -2);
+
+print("After append by array_unshift(\$array, -6, -4, -2):\n\n");
+print_r($array); print("\n");
