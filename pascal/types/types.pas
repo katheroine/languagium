0a15636ea2c5ffd5a0c8b4ed8ecde62381f88fab
katheroine <katheroine@gmail.com>
2021-09-13 11:12:31 +0200

    Pascal fundamental types [#types]

---
+++ pascal/types/types.pas
@@ -0,0 +1,54 @@
+program Types;
+
+var b: boolean;
+var ca: cardinal;
+var i: integer;
+var shi: shortint;
+var smi: smallint;
+var loi: longint;
+var i64: int64;
+var bt: byte;
+var w: word;
+var low: longword;
+
+begin
+  write('boolean b: ');
+  writeln(b);
+  writeln();
+
+  write('cardinal ca: ');
+  writeln(ca);
+  writeln();
+
+  write('integer i: ');
+  writeln(i);
+  writeln();
+
+  write('shortint shi: ');
+  writeln(shi);
+  writeln();
+
+  write('smallint smi: ');
+  writeln(smi);
+  writeln();
+
+  write('longint loi: ');
+  writeln(loi);
+  writeln();
+
+  write('int64 i64: ');
+  writeln(i64);
+  writeln();
+
+  write('byte bt: ');
+  writeln(bt);
+  writeln();
+
+  write('word w: ');
+  writeln(w);
+  writeln();
+
+  write('longword low: ');
+  writeln(low);
+  writeln();
+end.
