2a8affa36f8a2170de18e4692f20b2aa42926b77
katheroine <katheroine@gmail.com>
2021-09-16 16:20:47 +0200

    PHP array functions: crawlers [#arrays]

--- php/arrays/functions_operating_on_arrays/crawlers/array_walk_recursive.php
+++ php/arrays/functions_operating_on_arrays/crawlers/array_walk_recursive.php
@@ -3,36 +3,46 @@
 
 $array = ['apple', 'orange', 'banana', 'pear', 'peach', 'nested' => ['cherry', 'strawberry', 'blueberry', 'raspberry', 'blackberry']];
 
-array_walk_recursive($array, function($value, $key) {
+$result = array_walk_recursive($array, function($value, $key) {
   print("[{$key}]: {$value}\n");
 });
 
-print("\n");
+print("result:");
+print_r($result);
+print("\n\n");
 
 $presenter = function($value, $key) {
   print("element[{$key}]: {$value}\n");
 };
 
-array_walk_recursive($array, $presenter);
+$result = array_walk_recursive($array, $presenter);
 
-print("\n");
+print("result:");
+print_r($result);
+print("\n\n");
 
 function display_array($value, $key) {
   print("array[{$key}]: {$value}\n");
 }
 
-array_walk_recursive($array, 'display_array');
+$result = array_walk_recursive($array, 'display_array');
 
-print("\n");
+print("result:");
+print_r($result);
+print("\n\n");
 
 $array = [1, 3, 5, 'nested' => [2, 4]];
 
 print("Before:\n");
 print_r($array);
 
-array_walk_recursive($array, function(&$value, $key) {
+$result = array_walk_recursive($array, function(&$value, $key) {
   $value *= 10;
 });
 
 print("After:\n");
 print_r($array);
+
+print("result:");
+print_r($result);
+print("\n\n");
