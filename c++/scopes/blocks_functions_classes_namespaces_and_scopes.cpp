30e0e9472b1273d59c4a72332229f16a4fd16b2a
katheroine <katheroine@gmail.com>
2021-10-22 12:06:44 +0200

    C++ scopes [#scopes]

--- c++/scopes/blocks_functions_classes_namespaces_and_scopes.cpp
+++ c++/scopes/blocks_functions_classes_namespaces_and_scopes.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
+// g is global
 int g = 5;
 
 namespace some_namespace
 {
+  // n is local (in the namespace scope)
   int n = 10;
 }
 
 void some_function()
 {
+  // f is local (in the function scope)
   int f = 25;
   std::cout << f << std::endl;
 }
@@ -16,6 +19,7 @@ void some_function()
 class some_class
 {
 public:
+  // c is local (in the class scope)
   int c = 30;
   void some_function()
   {
@@ -29,10 +33,14 @@ int main()
 
   std::cout << some_namespace::n << std::endl;
 
+  // m is local (in the main function scope)
   int m = 15;
   std::cout << m << std::endl;
 
   {
+    // b is local (in the local block scope)
+    // this scope is nested in a way
+    // (it's the local block scope inside the main function scope)
     int b = 20;
     std::cout << b << std::endl;
   }
