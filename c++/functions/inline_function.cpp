928e1c2d3fdd27fb6b9d0c8ab289cf99dda89cc3
katheroine <katheroine@gmail.com>
2021-10-25 14:42:23 +0200

    C++ inline function [#functions]

---
+++ c++/functions/inline_function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+inline int round(double value)
+{
+  return (value + 0.5);
+}
+
+int main()
+{
+  std::cout << "round(3.14) = " << round(3.14) << std::endl;
+  std::cout << "round(2.72) = " << round(2.72) << std::endl;
+}
