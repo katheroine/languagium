49e958fb0638eb97efd82ef95573ff5724c6a902
katheroine <katheroine@gmail.com>
2021-11-04 14:54:00 +0100

    Python iterator internal pointers [#arrays]

---
+++ python/arrays/iterators/internal_pointers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+list = [1, 3, 5]
+iterator = iter(list)
+
+print(f"list: {list}")
+print(f"iterator: {iterator}\n")
+
+element = next(iterator)
+print(f"next(iterator): {element}")
+
+element = next(iterator)
+print(f"next(iterator): {element}")
+
+element = next(iterator)
+print(f"next(iterator): {element}")
+
+try:
+    element = next(iterator)
+    print(f"next(iterator): {element}")
+except StopIteration:
+    print("Cannot move forward.")
