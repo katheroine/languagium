f3666e8de52e249cd147e2ffd534f57469260e54
katheroine <katheroine@gmail.com>
2021-11-05 14:14:05 +0100

    Python associative arrays dictionary substitute [#arrays]

---
+++ python/arrays/dictionaries/associative_array_substitute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+items = {
+    2: "Hello, there!",
+    'color': 'orange',
+    3.14: 'PI',
+}
+
+print(f"items[2]: {items[2]}")
+print(f"items['color']: {items['color']}")
+print(f"items[3.14]: {items[3.14]}\n")
+
+print(items)
