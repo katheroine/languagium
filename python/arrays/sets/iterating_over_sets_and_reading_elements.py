507f371f96a1d9c139107857978a38468aa2e62e
katheroine <katheroine@gmail.com>
2021-11-11 14:16:56 +0100

    Python iterating over sets [#arrays]

---
+++ python/arrays/sets/iterating_over_sets_and_reading_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+set = {'apple', 'orange', 'orange', 'banana', 'pear', 'peach'}
+
+for key, value in enumerate(set):
+    print(f"element: {value} (key: {key})")
+
+print()
+
+for value in set:
+    print(f"element: {value}")
+
+print()
