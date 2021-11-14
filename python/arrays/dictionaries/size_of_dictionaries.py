c326d762d8457e7fee5d163c6e35c68836a84386
katheroine <katheroine@gmail.com>
2021-11-14 16:53:25 +0100

    Python size of dictionaries [#arrays]

---
+++ python/arrays/dictionaries/size_of_dictionaries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+numbers = {'a': 9, 'b': 7, 'c': 5}
+values = {'a': 9.5, 'b': 8.5, 'c': 7.5, 'd': 3.3, 'e': 2.0}
+
+size = len(numbers)
+print(f"Length of numbers: {size}")
+
+size = len(values)
+print(f"Length of values: {size}")
