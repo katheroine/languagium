2f4c315de0820c2c726db52cce8ed934568ad0c4
katheroine <katheroine@gmail.com>
2021-11-14 13:04:41 +0100

    JavaScript size of arrays [#arrays]

---
+++ javascript/arrays/size_of_arrays.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+let numbers = [9, 7, 5];
+let values = [9.5, 8.5, 7.5, 3.3, 2.0];
+let size;
+
+size = numbers.length;
+console.log(`Length of numbers: ${size}`);
+
+size = values.length;
+console.log(`Length of values: ${size}`);
