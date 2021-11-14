6dddb1470767ed23509cd01ae62eaae668f618f5
katheroine <katheroine@gmail.com>
2021-11-14 15:56:05 +0100

    Python size of lists [#arrays]

---
+++ python/arrays/lists/size_of_lists.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+numbers = [9, 7, 5]
+values = [9.5, 8.5, 7.5, 3.3, 2.0]
+
+size = len(numbers)
+print(f"Length of numbers: {size}")
+
+size = len(values)
+print(f"Length of values: {size}")
