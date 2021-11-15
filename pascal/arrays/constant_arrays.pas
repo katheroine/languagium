d413c9605ad6150ecbd763e59714cb5222aecf3c
katheroine <katheroine@gmail.com>
2021-11-15 19:32:43 +0100

    Pascal constant arrays [#arrays]

---
+++ pascal/arrays/constant_arrays.pas
@@ -0,0 +1,19 @@
+program Arrays;
+
+{$J-}
+const
+  numbers: array[0..2] of integer = (1, 3, 5);
+{$J+}
+
+var
+  values: array[0..2] of integer = (9, 8, 7);
+
+begin
+  writeln('numbers[0]: ', numbers[0]);
+  writeln('numbers[1]: ', numbers[1]);
+  writeln('numbers[2]: ', numbers[2]);
+  writeln();
+
+  // numbers[0] := 6; // error
+  // numbers := values; // error
+end.
