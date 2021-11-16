19eddb188e5ac2c944cc5014f9a147ad8aa4ab61
katheroine <katheroine@gmail.com>
2021-11-16 13:48:00 +0100

    Python set elements access [#arrays]

---
+++ python/arrays/sets/set_elements_access.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+numbers = {2, 4, 4, 6}
+
+iterator = iter(numbers)
+
+print(f"numbers element: {next(iterator)}")
+print(f"numbers element: {next(iterator)}")
+print(f"numbers element: {next(iterator)}\n")
