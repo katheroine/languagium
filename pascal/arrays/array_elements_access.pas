cdc78119ef828ed062dacca4f8d334f462a32313
katheroine <katheroine@gmail.com>
2021-11-16 10:48:16 +0100

    Pascal array elements access [#arrays]

---
+++ pascal/arrays/array_elements_access.pas
@@ -0,0 +1,24 @@
+program Arrays;
+
+var
+  numbers: array[0..2] of integer;
+  values: ^integer;
+
+begin
+  // Array elements must be initialised before access.
+  numbers[0] := 2;
+  numbers[1] := 4;
+  numbers[2] := 6;
+
+  writeln('numbers[0]: ', numbers[0]);
+  writeln('numbers[1]: ', numbers[1]);
+  writeln('numbers[2]: ', numbers[2]);
+  writeln();
+
+  values := numbers;
+
+  writeln('numbers[0]: ', values^);
+  writeln('numbers[1]: ', values[1]);
+  writeln('numbers[2]: ', (values + 2)^);
+  writeln();
+end.
