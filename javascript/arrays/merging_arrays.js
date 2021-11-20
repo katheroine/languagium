3c15f0843cfb20462148702b9cbdce73213e87e4
katheroine <katheroine@gmail.com>
2021-11-20 17:17:02 +0100

    JavaScript merging arrays examples modified [#arrays]

--- javascript/arrays/merging_arrays.js
+++ javascript/arrays/merging_arrays.js
@@ -1,17 +1,12 @@
 #!/usr/bin/env node
 
-let values = [9.5, 8.5, 7.5];
-let items = [2, "orange"];
+let values = [9.5, 2.5, 7.5];
+let items = [2.5, "orange"];
 
 console.log(`values: ${values}`);
 console.log(`items: ${items}\n`);
 
-let elements = values + items;
-
-console.log(`elements = values + items`);
-console.log(`elements: ${elements}\n`);
-
-elements = items.concat(values);
+let elements = items.concat(values);
 
 console.log(`elements = items.concat(values)`);
 console.log(`elements: ${elements}\n`);
