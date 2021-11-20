fbf1ca15175d0b581f336810dbdfb3519bfd5d6a
katheroine <katheroine@gmail.com>
2021-11-20 13:19:31 +0100

    PHP merging arrays [#arrays]

---
+++ php/arrays/merging_arrays.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php8.0
+<?php
+
+$values = [9.5, 8.5, 7.5];
+$items = [2, "orange"];
+
+print("values:\n");
+print_r($values);
+print("items:\n");
+print_r($items);
+print("\n");
+
+$elements = array_merge($values, $items);
+
+print("\$elements = array_merge(\$values, \$items)\n");
+print("elements:\n");
+print_r($elements);
+print("\n");
+
+$elements = $items + $values;
+
+print("\$elements = \$items + \$values\n");
+print("elements:\n");
+print_r($elements);
+print("\n");
