893d103b47d51b2a0c93b690f33db9fdcf1eda0c
katheroine <katheroine@gmail.com>
2021-11-21 09:40:11 +0100

    Python merging tuples [#arrays]

---
+++ python/arrays/tuples/merging_tuples.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+values = (9.5, 2.5, 7.5)
+items = (2.5, "orange")
+
+print(f"values: {values}")
+print(f"items: {items}\n")
+
+elements = values + items
+
+print(f"elements = values + items")
+print(f"elements: {elements}\n")
