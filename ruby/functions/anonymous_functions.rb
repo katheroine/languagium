c303d0fcc659d90c2769af082ef66d6919e5a201
katheroine <katheroine@gmail.com>
2021-11-26 17:38:21 +0100

    Ruby anonymous functions [#functions]

---
+++ ruby/functions/anonymous_functions.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby2.7
+
+simple_function = -> { puts "Simple function." }
+
+function_with_local_variable = -> {
+  i = 4
+  puts("A function with local variable: #{i}")
+}
+
+function_returning_value = -> {
+  puts("A function returning value.")
+  9
+}
+
+function_with_arguments = ->(number, text) {
+  puts("A function with some arguments:\nnumber: #{number}\ntext: #{text}")
+}
+
+i = 10
+
+puts "Functions:\n\n"
+
+simple_function.call
+puts
+
+function_with_local_variable.call
+puts
+
+i = function_returning_value.call
+puts "returned value: #{i}\n\n"
+
+function_with_arguments.call(6, "orange")
+puts
