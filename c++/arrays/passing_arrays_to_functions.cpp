50e9925442d285f30b51c2976ed5321493c7add5
katheroine <katheroine@gmail.com>
2021-11-29 13:43:26 +0100

    C++ passing arrays to the functions [#arrays]

---
+++ c++/arrays/passing_arrays_to_functions.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+void function_receiving_array_by_pointer(int argument[], int size)
+{
+  std::cout << "Function receiving array by value\n"
+    << "-- begin:\n";
+
+  for(int i = 0; i < size; i++)
+  {
+    std::cout << "before: argument[" << i << "] = " << argument[i] << std::endl
+    << "argument[" << i << "] = argument[" << i << "] * 2\n";
+
+    argument[i] *= 2;
+
+    std::cout << "after: arguemnt[" << i << "] = " << argument[i] << std::endl;
+  }
+
+  std::cout << "-- end.\n";
+}
+
+void function_receiving_array_by_explicit_pointer(int *argument, int size)
+{
+  std::cout << "Function receiving array by value\n"
+    << "-- begin:\n";
+
+  for(int i = 0; i < size; i++)
+  {
+    std::cout << "before: argument[" << i << "] = " << argument[i] << std::endl
+    << "argument[" << i << "] = argument[" << i << "] * 2\n";
+
+    argument[i] *= 2;
+
+    std::cout << "after: arguemnt[" << i << "] = " << argument[i] << std::endl;
+  }
+
+  std::cout << "-- end.\n";
+}
+
+int main()
+{
+  int values[] = {9, 8, 7};
+
+  std::cout << "BEFORE: values = [ ";
+  for (int element : values)
+    std::cout << element << " ";
+  std::cout << "]\n";
+
+  function_receiving_array_by_pointer(values, 3);
+
+  std::cout << "AFTER: values = [ ";
+  for (int element : values)
+    std::cout << element << " ";
+  std::cout << "]\n\n";
+
+  values[0] = 9;
+  values[1] = 8;
+  values[2] = 7;
+
+  std::cout << "BEFORE: values = [ ";
+  for (int element : values)
+    std::cout << element << " ";
+  std::cout << "]\n";
+
+  function_receiving_array_by_explicit_pointer(values, 3);
+
+  std::cout << "AFTER: values = [ ";
+  for (int element : values)
+    std::cout << element << " ";
+  std::cout << "]\n\n";
+}
