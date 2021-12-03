9b3271ef944f58f2e2fe76f41d1250c506b2dffe
katheroine <katheroine@gmail.com>
2021-12-03 16:39:26 +0100

    JavaScript types example completed [#types]

--- javascript/types/types.js
+++ javascript/types/types.js
@@ -5,9 +5,19 @@ let b = true;
 let i = 5;
 let f = 2.4;
 let s = "hello";
+let a = [3, 5, 7];
+let o = {
+  2: "Hello, there!",
+  color: 'orange',
+  3.14: 'PI',
+}
 
 console.log("null: " + n + " (" + typeof n + ")");
-console.log("null: " + b + " (" + typeof b + ")");
-console.log("null: " + i + " (" + typeof i + ")");
-console.log("null: " + f + " (" + typeof f + ")");
-console.log("null: " + s + " (" + typeof s + ")");
+console.log("bool: " + b + " (" + typeof b + ")");
+console.log("integer: " + i + " (" + typeof i + ")");
+console.log("floating point: " + f + " (" + typeof f + ")");
+console.log("string: " + s + " (" + typeof s + ")");
+console.log("array: " + a + " (" + typeof a + ")");
+console.log("object: ");
+console.log(o);
+console.log("(" + typeof o + ")");
