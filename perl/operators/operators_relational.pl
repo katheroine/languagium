9b4e00b4487be95f981edfb3a586fb9c378be7b2
katheroine <katheroine@gmail.com>
2021-12-09 13:46:26 +0100

    Perl relational operator example modified [#operators]

--- perl/operators/operators_relational.pl
+++ perl/operators/operators_relational.pl
@@ -3,7 +3,9 @@
 use strict;
 use warnings;
 
-my $a = 1; my $b = 2; my $c = false;
+my $a; my $b; my $c;
+
+$a = 1; $b = 2;
 
 $c = $a < $b;
 print("$a < $b = $c\n");
@@ -20,3 +22,25 @@ print("$a != $b = $c\n");
 
 $c = $a <=> $b;
 print("$a <=> $b = $c\n");
+
+print("\n");
+
+$a = "abc", $b = "def";
+
+$c = $a lt $b;
+print("$a lt $b = $c\n");
+$c = $a gt $b;
+print("$a gt $b = $c\n");
+$c = $a le $b;
+print("$a le $b = $c\n");
+$c = $a ge $b;
+print("$a ge $b = $c\n");
+$c = $a eq $b;
+print("$a eq $b = $c\n");
+$c = $a ne $b;
+print("$a ne $b = $c\n");
+
+$c = $a cmp $b;
+print("$a cmp $b = $c\n");
+
+print("\n");
