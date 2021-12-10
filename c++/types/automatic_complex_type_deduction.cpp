991bc8529bf7caff233c0fc211109c1850aed8c0
katheroine <katheroine@gmail.com>
2021-12-10 11:11:16 +0100

    C++ types examples modified [#types]

--- c++/types/automatic_complex_type_deduction.cpp
+++ c++/types/automatic_complex_type_deduction.cpp
@@ -6,12 +6,12 @@ int main()
   // Array
   auto a = {3, 5, 7};
   // Pointer
-  float d = 1.1;
-  auto p = &d;
+  float f = 1.1;
+  auto p = &f;
   // Function
-  auto f = []() { return 3; };
+  auto u = [](int number) { return number * 3; };
 
   std::cout << "a = {3, 5, 7}: " << typeid(a).name() << std::endl;
   std::cout << "p = &d: " << typeid(p).name() << std::endl;
-  std::cout << "f = [](){ return 3; }: " << typeid(f).name() << std::endl;
+  std::cout << "u = [](int number) { return number * 3; }: " << typeid(u).name() << std::endl;
 }
--- c++/types/complex_types.cpp
+++ c++/types/complex_types.cpp
@@ -4,13 +4,13 @@
 int main()
 {
   // Array
-  int a[10];
+  int a[3];
   // Pointer
   float *p;
   // Function
-  char f();
+  int u();
 
   std::cout << "int a[]: " << typeid(a).name() << std::endl;
   std::cout << "float *p: " << typeid(p).name() << std::endl;
-  std::cout << "char f(): " << typeid(f).name() << std::endl;
+  std::cout << "int u(): " << typeid(u).name() << std::endl;
 }
