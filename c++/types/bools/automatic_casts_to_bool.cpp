41479c490b60c19c972ea8248c3d4d26d5bc82b8
katheroine <katheroine@gmail.com>
2021-12-12 14:24:18 +0100

    C++ automatic casts to bool example modified [#types]

--- c++/types/bools/automatic_casts_to_bool.cpp
+++ c++/types/bools/automatic_casts_to_bool.cpp
@@ -3,108 +3,134 @@
 int main()
 {
   int i;
+  std::cout << "int i;" << std::endl << std::endl;
 
   i = 0;
-  if (i) std::cout << "int " << i << " casted to true" << std::endl;
-  else std::cout << "int " << i << " casted to false" << std::endl;
+  std::cout << "i = 0;" << std::endl;
+  if (i) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   i = 1;
-  if (i) std::cout << "int " << i << " casted to true" << std::endl;
-  else std::cout << "int " << i << " casted to false" << std::endl;
+  std::cout << "i = 1;" << std::endl;
+  if (i) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   i = 3;
-  if (i) std::cout << "int " << i << " casted to true" << std::endl;
-  else std::cout << "int " << i << " casted to false" << std::endl;
+  std::cout << "i = 3;" << std::endl;
+  if (i) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   i = -3;
-  if (i) std::cout << "int " << i << " casted to true" << std::endl;
-  else std::cout << "int " << i << " casted to false" << std::endl;
+  std::cout << "i = -3;" << std::endl;
+  if (i) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   std::cout << std::endl;
 
   float f;
+  std::cout << "float f;" << std::endl << std::endl;
 
   f = 0;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = 0;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   f = 1;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = 1;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << " casted to false" << std::endl;
 
   f = 3;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = 3;" << std::endl;
+  if (f) std::cout << " casted to true" << std::endl;
+  else std::cout << " casted to false" << std::endl;
 
   f = -3;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = -3;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   f = 0.1;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = 0.1;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   f = 3.14;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = 3.14;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   f = -3.14;
-  if (f) std::cout << "float " << f << " casted to true" << std::endl;
-  else std::cout << "float " << f << " casted to false" << std::endl;
+  std::cout << "f = -3.14;" << std::endl;
+  if (f) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   std::cout << std::endl;
 
   char c;
+  std::cout << "char c;" << std::endl << std::endl;
 
   c = '\0';
-  if (c) std::cout << "char '" << c << "' [null (\\0) character (code: " << (int) c << ")] casted to true" << std::endl;
-  else std::cout << "char '" << c << "' [null (\\0) character (code: " << (int) c << ")] casted to false" << std::endl;
+  std::cout << "c = '\\0'; // null (\\0) character (code: " << (int) c << ")" << std::endl;
+  if (c) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   // c = ''; // error
 
   c = ' ';
-  if (c) std::cout << "char '" << c << "' [space character] casted to true" << std::endl;
-  else std::cout << "char '" << c << "' [space character] casted to false" << std::endl;
+  std::cout << "c = ' '; // space character (code: " << (int) c << ")" << std::endl;
+  if (c) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   c = 'a';
-  if (c) std::cout << "char '" << c << "' casted to true" << std::endl;
-  else std::cout << "char '" << c << "' casted to false" << std::endl;
+  std::cout << "c = 'a'; // (code: " << (int) c << ")" << std::endl;
+  if (c) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   c = '0';
-  if (c) std::cout << "char '" << c << "' casted to true" << std::endl;
-  else std::cout << "char '" << c << "' casted to false" << std::endl;
+  std::cout << "c = '0'; // (code: " << (int) c << ")" << std::endl;
+  if (c) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   c = '1';
-  if (c) std::cout << "char '" << c << "' casted to true" << std::endl;
-  else std::cout << "char '" << c << "' casted to false" << std::endl;
+  std::cout << "c = '1'; // (code: " << (int) c << ")" << std::endl;
+  if (c) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   std::cout << std::endl;
 
   const char* s;
+  std::cout << "const char* s; // c-string" << std::endl << std::endl;
 
   s = "\0";
-  if (s) std::cout << "string \"" << s << "\" [null (\\0) character (code: " << (int) s[0] << ")] casted to true" << std::endl;
-  else std::cout << "string '" << s << "' [null (\\0) character (code: " << (int) s[0] << ")] casted to false" << std::endl;
+  std::cout << "s = \"\\0\"; // null (\\0) character (code: " << (int) s[0] << ")" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   s = "";
-  if (s) std::cout << "string \"" << s << "\" [empty string - if fact: null (\\0) character (code: " << (int) s[0] << ")] casted to true" << std::endl;
-  else std::cout << "string '" << s << "' [empty string - in fact: null (\\0) character (code: " << (int) s[0] << ")] casted to false" << std::endl;
+  std::cout << "s = \"\"; // empty string - if fact: null (\\0) character (code: " << (int) s[0] << ")" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   s = " ";
-  if (s) std::cout << "string \"" << s << "\" [space character] casted to true" << std::endl;
-  else std::cout << "string \"" << s << "\" [space character] casted to false" << std::endl;
+  std::cout << "s = \" \"; // space character (code: " << (int) s[0] << ")" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   s = "a";
-  if (s) std::cout << "string \"" << s << "\" casted to true" << std::endl;
-  else std::cout << "string \"" << s << "\" casted to false" << std::endl;
+  std::cout << "s = \"a\";" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   s = "false";
-  if (s) std::cout << "string \"" << s << "\" casted to true" << std::endl;
-  else std::cout << "string \"" << s << "\" casted to false" << std::endl;
+  std::cout << "s = \"false\";" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   s = "true";
-  if (s) std::cout << "string \"" << s << "\" casted to true" << std::endl;
-  else std::cout << "string \"" << s << "\" casted to false" << std::endl;
+  std::cout << "s = \"true\";" << std::endl;
+  if (s) std::cout << "casted to true" << std::endl;
+  else std::cout << "casted to false" << std::endl;
 
   std::cout << std::endl;
 }
