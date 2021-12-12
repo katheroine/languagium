3ad991209ec1e8ff514dc979ce08d6d78fb596ef
katheroine <katheroine@gmail.com>
2021-12-12 22:15:23 +0100

    C++ splitting c-strings in notation example modified [#types]

--- c++/types/cstrings/splitting_cstrings_in_notation.cpp
+++ c++/types/cstrings/splitting_cstrings_in_notation.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-  std::cout << "Hello! I'm simple c-string " << std::endl;
-  std::cout << "And this is the c-string with and specian character - new line ant it's end.\n";
+  std::cout << "Hello! I'm simple c-string." << std::endl;
+  std::cout << "And this is the c-string with and special character - new line ant it's end.\n";
   std::cout << "This is c-string "
     "splitted into several lines "
     "on it's notation.\n";
