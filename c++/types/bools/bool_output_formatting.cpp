6a0ede036a0ed519389d96a23d0be0cf5348591d
katheroine <katheroine@gmail.com>
2021-12-12 14:23:51 +0100

    C++ bool output formatting example modified [#types]

--- c++/types/bools/bool_output_formatting.cpp
+++ c++/types/bools/bool_output_formatting.cpp
@@ -2,44 +2,62 @@
 
 int main()
 {
-  std::cout << true << std::endl
-    << false << std::endl
+  std::cout << "boolean literals: " << std::endl
+    << "default mode: " << std::endl
+    << "true: " << true << std::endl
+    << "false: " << false << std::endl
+    << "std::boolalpha mode: " << std::endl
     << std::boolalpha
-    << true << std::endl
-    << false << std::endl
-    << std::noboolalpha;
+    << "true: " << true << std::endl
+    << "false: " << false << std::endl
+    << std::noboolalpha << std::endl;
+
+  std::cout << "integer literals: " << std::endl
+    << "default mode: " << std::endl
+    << "1: " << 1 << std::endl
+    << "0: " << 0 << std::endl
+    << "std::boolalpha mode: " << std::endl
+    << std::boolalpha
+    << "1: " << 1 << std::endl
+    << "0: " << 0 << std::endl
+    << std::noboolalpha << std::endl;
 
   bool a = true, b = false;
 
-  std::cout << a << std::endl
-    << b << std::endl
+  std::cout << "boolean variables: " << std::endl
+    << "bool a = true, b = false;" << std::endl
+    << "default mode: " << std::endl
+    << "a = " << a << std::endl
+    << "b = " << b << std::endl
+    << "std::boolalpha mode: " << std::endl
     << std::boolalpha
-    << a << std::endl
-    << b << std::endl
-    << std::noboolalpha;
+    << "a = " << a << std::endl
+    << "b = " << b << std::endl
+    << std::noboolalpha << std::endl;
 
   bool c = 1, d = 0;
 
-  std::cout << c << std::endl
-    << d << std::endl
+  std::cout << "boolean variables: " << std::endl
+    << "bool c = 1, d = 0;" << std::endl
+    << "default mode: " << std::endl
+    << "c: " << c << std::endl
+    << "d: " << d << std::endl
+    << "std::boolalpha mode: " << std::endl
     << std::boolalpha
-    << c << std::endl
-    << d << std::endl
-    << std::noboolalpha;
-
-  std::cout << 1 << std::endl
-    << 0 << std::endl
-    << std::boolalpha
-    << 1 << std::endl
-    << 0 << std::endl
-    << std::noboolalpha;
+    << "c: " << c << std::endl
+    << "d: " << d << std::endl
+    << std::noboolalpha << std::endl;
 
   int e = 0, f = 1;
 
-  std::cout << e << std::endl
-    << f << std::endl
+  std::cout << "integer variables: " << std::endl
+    << "int e = 0, f = 1;" << std::endl
+    << "default mode: " << std::endl
+    << "e: " << e << std::endl
+    << "f: " << f << std::endl
+    << "std::boolalpha mode: " << std::endl
     << std::boolalpha
-    << e << std::endl
-    << f << std::endl
-    << std::noboolalpha;
+    << "e: " << e << std::endl
+    << "f: " << f << std::endl
+    << std::noboolalpha << std::endl;
 }
