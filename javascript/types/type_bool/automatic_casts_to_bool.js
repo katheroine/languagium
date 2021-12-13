25b5990a5ac0e9777263f463f8fcd68dbfa42fbc
katheroine <katheroine@gmail.com>
2021-12-13 15:23:02 +0100

    JavaScript automatic casts to bool example modified [#types]

--- javascript/types/type_bool/automatic_casts_to_bool.js
+++ javascript/types/type_bool/automatic_casts_to_bool.js
@@ -1,92 +1,92 @@
 #!/usr/bin/env node
 
 let i = 0;
-
-if (i) console.log("i = " + i + " (" + typeof i + ") casted to true");
-else console.log("i = " + i + " (" + typeof i + ") casted to false");
+console.log("let i = 0; // (" + typeof i + ")");
+if (i) console.log("casted to true");
+else console.log("casted to false");
 
 i = 1;
-
-if (i) console.log("i = " + i + " (" + typeof i + ") casted to true");
-else console.log("i = " + i + " (" + typeof i + ") casted to false");
+console.log("i = 1; // (" + typeof i + ")");
+if (i) console.log("casted to true");
+else console.log("casted to false");
 
 i = 3;
-
-if (i) console.log("i = " + i + " (" + typeof i + ") casted to true");
-else console.log("i = " + i + " (" + typeof i + ") casted to false");
+console.log("i = 3; // (" + typeof i + ")");
+if (i) console.log("casted to true");
+else console.log("casted to false");
 
 i = -3;
-
-if (i) console.log("i = " + i + " (" + typeof i + ") casted to true");
-else console.log("i = " + i + " (" + typeof i + ") casted to false");
+console.log("i = -3; // (" + typeof i + ")");
+if (i) console.log("casted to true");
+else console.log("casted to false");
 
 console.log();
 
 let f = 0.0;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("let f = 0.0; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = 1.0;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = 1.0; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = 3.0;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = 3.0; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = -3.0;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = -3.0; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = 0.1;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = 0.1; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = 3.14;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = 3.14; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 f = -3.14;
-
-if (f) console.log("f = " + f + " (" + typeof f + ") casted to true");
-else console.log("f = " + f + " (" + typeof f + ") casted to false");
+console.log("f = -3.14; // (" + typeof f + ")");
+if (f) console.log("casted to true");
+else console.log("casted to false");
 
 console.log()
 
 let s = "\0";
-
-if (s) console.log("s = " + s + "[null (\\0) character] (" + typeof s + ") casted to true");
-else console.log("s = " + s + "[null (\\0) character] (" + typeof s + ") casted to false");
+console.log("let s = \"\\0\"; // null (\\0) character (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 s = "";
-
-if (s) console.log("s = " + s + "[empty string] (" + typeof s + ") casted to true");
-else console.log("s = " + s + "[empty string] (" + typeof s + ") casted to false");
+console.log("s = \"\"; // empty string (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 s = " ";
-
-if (s) console.log("s = " + s + "[space] (" + typeof s + ") casted to true");
-else console.log("s = " + s + "[space] (" + typeof s + ") casted to false");
+console.log("s = \" \"; // space (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 s = "a";
-
-if (s) console.log("s = " + s + " (" + typeof s + ") casted to true");
-else console.log("s = " + s + " (" + typeof s + ") casted to false");
+console.log("s = \"a\"; // (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 s = "false";
-
-if (s) console.log("s = " + s + " (" + typeof s + ") casted to true");
-else console.log("s = " + s + " (" + typeof s + ") casted to false");
+console.log("s = \"false\"; // (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 s = "true";
-
-if (s) console.log("s = " + s + " (" + typeof s + ") casted to true");
-else console.log("s = " + s + " (" + typeof s + ") casted to false");
+console.log("s = \"true\"; // (" + typeof s + ")");
+if (s) console.log("casted to true");
+else console.log("casted to false");
 
 console.log();
