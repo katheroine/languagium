e5a7bee7ef8c3ae598d95c941e30f92f2524a2e2
katheroine <katheroine@gmail.com>
2021-12-13 18:17:22 +0100

    JavaScript string length [#types]

---
+++ javascript/types/type_string/string_length.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+let word = "hello";
+console.log("word = " + word + ", length: " + s.length + "\n");
+
+let cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+console.log("cite = " + cite + ", length: " + cite);
+console.log("words quantity: " + cite.split(' ').length + "\n");
