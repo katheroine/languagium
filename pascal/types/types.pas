3445918f04e510b2c58858f9de8a00a052bdd879
katheroine <katheroine@gmail.com>
2021-12-13 11:25:42 +0100

    Pascal types example modified [#types]

--- pascal/types/types.pas
+++ pascal/types/types.pas
@@ -1,25 +1,29 @@
 program Types;
 
+// Boolean
 var b: boolean;
 var ca: cardinal;
+// Integers
 var i: integer;
 var shi: shortint;
 var smi: smallint;
 var loi: longint;
 var i64: int64;
+// Byte
 var bt: byte;
+// Strings
 var w: word;
 var low: longword;
 
 begin
-  writeln('boolean b: ', b);
-  writeln('cardinal ca: ', ca);
-  writeln('integer i: ', i);
-  writeln('shortint shi: ', shi);
-  writeln('smallint smi: ', smi);
-  writeln('longint loi: ', loi);
-  writeln('int64 i64: ', i64);
-  writeln('byte bt: ', bt);
-  writeln('word w: ', w);
-  writeln('longword low: ', low);
+  writeln('b: boolean; // = ', b);
+  writeln('ca: cardinal; // = ', ca);
+  writeln('i: integer; // = ', i);
+  writeln('shi: shortint; // = ', shi);
+  writeln('smi: smallint; // = ', smi);
+  writeln('loi: longint; // = ', loi);
+  writeln('i64: int64; // = ', i64);
+  writeln('bt: byte; // = ', bt);
+  writeln('w: word; // = ', w);
+  writeln('low: longword; // = ', low);
 end.
