0cb8ea50607409391441e9d233b14258eed6dad9
katheroine <katheroine@gmail.com>
2021-12-13 12:57:39 +0100

    JavaScript types example modified [#types]

--- javascript/types/types.js
+++ javascript/types/types.js
@@ -13,15 +13,13 @@ let o = {
   2: "Hello, there!",
   color: 'orange',
   3.14: 'PI',
-}
+};
 
-console.log("null: " + n + " (" + typeof n + ")\n");
-console.log("bool: " + b + " (" + typeof b + ")\n");
-console.log("integer: " + i + " (" + typeof i + ")\n");
-console.log("floating point: " + f + " (" + typeof f + ")\n");
-console.log("string: " + s + " (" + typeof s + ")\n");
-console.log("array: " + a + " (" + typeof a + ")\n");
-console.log("function: " + u + "\n(" + typeof u + ")\n");
-console.log("object: ");
-console.log(o);
-console.log("(" + typeof o + ")\n");
+console.log("let n = null; // null: " + n + " (" + typeof n + ")\n");
+console.log("let b = true; // boolean: " + b + " (" + typeof b + ")\n");
+console.log("let i = 5; // integer: " + i + " (" + typeof i + ")\n");
+console.log("let f = 2.4; // floating point: " + f + " (" + typeof f + ")\n");
+console.log("let s = \"hello\"; // string: " + s + " (" + typeof s + ")\n");
+console.log("let a = [3, 5, 7]; // array: " + a + " (" + typeof a + ")\n");
+console.log("let u = function(number) {\n  return number * 3;\n};\n// function: " + u + "\n(" + typeof u + ")\n");
+console.log("let o = {\n  2: \"Hello, there!\",\n  color: 'orange',\n  3.14: 'PI',\n};\n// object: ", o, "(" + typeof o + ")\n");
