dd1530c537cff709b1408a968db58aee0dca675b
katheroine <katheroine@gmail.com>
2021-12-14 12:12:52 +0100

    Ruby automatic casts to bool example modified [#types]

--- ruby/types/type_bool/automatic_casts_to_bool.rb
+++ ruby/types/type_bool/automatic_casts_to_bool.rb
@@ -1,92 +1,92 @@
 #!/usr/bin/ruby2.7
 
 i = 0
-
-if i then puts "i = #{i} (#{i.class}) casted to true"
-else puts "i = #{i} (#{i.class}) casted to false" end
+puts "i = 0 # (#{i.class})"
+if i then puts "casted to true"
+else puts "casted to false" end
 
 i = 1
-
-if i then puts "i = #{i} (#{i.class}) casted to true"
-else puts "i = #{i} (#{i.class}) casted to false" end
+puts "i = 1 # (#{i.class})"
+if i then puts "casted to true"
+else puts "casted to false" end
 
 i = 3
-
-if i then puts "i = #{i} (#{i.class}) casted to true"
-else puts "i = #{i} (#{i.class}) casted to false" end
+puts "i = 3 # (#{i.class})"
+if i then puts "casted to true"
+else puts "casted to false" end
 
 i = -3
-
-if i then puts "i = #{i} (#{i.class}) casted to true"
-else puts "i = #{i} (#{i.class}) casted to false" end
+puts "i = -3 # (#{i.class})"
+if i then puts "casted to true"
+else puts "casted to false" end
 
 puts
 
 f = 0.0
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = 0.0 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = 1.0
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = 1.0 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = 3.0
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = 3.0 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = -3.0
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = -3.0 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = 0.1
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = 0.1 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = 3.14
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = 3.14 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 f = -3.14
-
-if f then puts "f = #{f} (#{f.class}) casted to true"
-else puts "f = #{f} (#{f.class}) casted to false" end
+puts "f = -3.14 # (#{f.class})"
+if f then puts "casted to true"
+else puts "casted to false" end
 
 puts
 
 s = "\0"
-
-if s then puts "s = #{s} [null (\\0) character] (#{s.class}) casted to true"
-else puts "i = #{s} [null (\\0) character] (#{s.class}) casted to false" end
+puts "s = \"\\0\" # null (\\0) character (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 s = ""
-
-if s then puts "s = #{s} [empty string] (#{s.class}) casted to true"
-else puts "i = #{s} [empty string] (#{s.class}) casted to false" end
+puts "s = \"\" # empty string (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 s = " "
-
-if s then puts "s = #{s} [space] (#{s.class}) casted to true"
-else puts "i = #{s} [space] (#{s.class}) casted to false" end
+puts "s = \" \" # space (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 s = "a"
-
-if s then puts "s = #{s} (#{s.class}) casted to true"
-else puts "i = #{s} (#{s.class}) casted to false" end
+puts "s = \"a\" # (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 s = "false"
-
-if s then puts "s = #{s} (#{s.class}) casted to true"
-else puts "i = #{s} (#{s.class}) casted to false" end
+puts "s = \"false\" # (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 s = "true"
-
-if s then puts "s = #{s} (#{s.class}) casted to true"
-else puts "i = #{s} (#{s.class}) casted to false" end
+puts "s = \"true\" # (#{f.class})"
+if s then puts "casted to true"
+else puts "casted to false" end
 
 puts
