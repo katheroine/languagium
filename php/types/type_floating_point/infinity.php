1a1a151537608671c08c8bb3ea541dbeb0b86d87
katheroine <katheroine@gmail.com>
2021-12-15 00:00:35 +0100

    PHP infinity example modified [#types]

--- php/types/type_floating_point/infinity.php
+++ php/types/type_floating_point/infinity.php
@@ -1,8 +1,16 @@
 #!/usr/bin/php8.0
 <?php
 
-$x = 1.9e411;
-print "{$x}: " . gettype($x) . "\n";
-var_dump($x);
-print "is finite: " . is_finite($x) . "\n";
-print "is infinite: " . is_infinite($x) . "\n";
+$positive_infinity = 1.9e411;
+print "\$positive_infinity = 1.9e411; // {$positive_infinity} (" . gettype($positive_infinity) . ")\n";
+
+$negative_infinity = -1.9e411;
+print "\$negative_infinity = 1.9e411; // {$negative_infinity} (" . gettype($negative_infinity) . ")\n";
+
+print "\n";
+
+print "is_finite($positive_infinity): " . is_finite($positive_infinity) . "\n";
+print "is_infinite($positive_infinity): " . is_infinite($positive_infinity) . "\n";
+
+print "is_finite($negative_infinity): " . is_finite($negative_infinity) . "\n";
+print "is_infinite($negative_infinity): " . is_infinite($negative_infinity) . "\n";
