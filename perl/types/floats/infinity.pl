ac0fff6d6c5108385499e56c69506d5751f5cde4
katheroine <katheroine@gmail.com>
2021-12-15 00:12:47 +0100

    Perl infinity [#types]

---
+++ perl/types/floats/infinity.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $positive_infinity = 9**9**9;
+print "my \$positive_infinity = 9**9**9; // {$positive_infinity}\n";
+
+my $negative_infinity = -9**9**9;
+print "my \$negative_infinity = -9**9**9; // {$negative_infinity}\n";
