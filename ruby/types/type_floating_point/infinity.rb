8b3908a1d4cadae8e932df0447e318ce1348d908
katheroine <katheroine@gmail.com>
2021-12-15 00:01:17 +0100

    Python infinity [#types]

---
+++ ruby/types/type_floating_point/infinity.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby2.7
+
+positive_infinity = Float::INFINITY
+puts("positive_infinity = Float::INFINITY # #{positive_infinity} (#{positive_infinity.class})")
+
+negative_infinity = -Float::INFINITY
+puts("negative_infinity = -Float::INFINITY # #{negative_infinity} (#{negative_infinity.class})")
