7b0a2274b0beef61c77ade1c057abad9f57290fb
katheroine <katheroine@gmail.com>
2021-12-14 11:15:55 +0100

    PHP automatic casts to bool example modified [#types]

--- php/types/type_bool/automatic_casts_to_bool.php
+++ php/types/type_bool/automatic_casts_to_bool.php
@@ -2,92 +2,92 @@
 <?php
 
 $i = 0;
-
-if ($i) print "\$i = $i (" . gettype($i) . ") casted to true\n";
-else print "\$i = $i (" . gettype($i) . ") casted to false\n";
+print("\$i = 0; // (" . gettype($i) . ")\n");
+if ($i) print "casted to true\n";
+else print "casted to false\n";
 
 $i = 1;
-
-if ($i) print "\$i = $i (" . gettype($i) . ") casted to true\n";
-else print "\$i = $i (" . gettype($i) . ") casted to false\n";
+print("\$i = 1; // (" . gettype($i) . ")\n");
+if ($i) print "casted to true\n";
+else print "casted to false\n";
 
 $i = 3;
-
-if ($i) print "\$i = $i (" . gettype($i) . ") casted to true\n";
-else print "\$i = $i (" . gettype($i) . ") casted to false\n";
+print("\$i = 3; // (" . gettype($i) . ")\n");
+if ($i) print "casted to true\n";
+else print "casted to false\n";
 
 $i = -3;
-
-if ($i) print "\$i = $i (" . gettype($i) . ") casted to true\n";
-else print "\$i = $i (" . gettype($i) . ") casted to false\n";
+print("\$i = -3; // (" . gettype($i) . ")\n");
+if ($i) print "casted to true\n";
+else print "casted to false\n";
 
 print "\n";
 
 $d = 0.0;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = 0.0; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = 1.0;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = 1.0; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = 3.0;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = 3.0; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = -3.0;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = -3.0; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = 0.1;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = 0.1; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = 3.14;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = 3.14; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 $d = -3.14;
-
-if ($d) print "\$d = $d (" . gettype($d) . ") casted to true\n";
-else print "\$d = $d (" . gettype($d) . ") casted to false\n";
+print("\$d = -3.14; // (" . gettype($d) . ")\n");
+if ($d) print "casted to true\n";
+else print "casted to false\n";
 
 print "\n";
 
 $s = "\0";
-
-if ($s) print "\$s = $s [null (\\0) character] (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s [null (\\0) character] (" . gettype($s) . ") casted to false\n";
+print("\$s = \"\\0\"; // null (\\0) character (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 $s = "";
-
-if ($s) print "\$s = $s [empty string] (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s [empty string] (" . gettype($s) . ") casted to false\n";
+print("\$s = \"\"; // empty string (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 $s = " ";
-
-if ($s) print "\$s = $s [space] (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s [space] (" . gettype($s) . ") casted to false\n";
+print("\$s = \" \"; // space (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 $s = "a";
-
-if ($s) print "\$s = $s (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s (" . gettype($s) . ") casted to false\n";
+print("\$s = \"a\"; // (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 $s = "false";
-
-if ($s) print "\$s = $s (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s (" . gettype($s) . ") casted to false\n";
+print("\$s = \"false\"; // (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 $s = "true";
-
-if ($s) print "\$s = $s (" . gettype($s) . ") casted to true\n";
-else print "\$s = $s (" . gettype($s) . ") casted to false\n";
+print("\$s = \"true\"; // (" . gettype($s) . ")\n");
+if ($s) print "casted to true\n";
+else print "casted to false\n";
 
 print "\n";
