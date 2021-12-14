fc5c43a5a4d9702054c62b04be1bed5c756a8151
katheroine <katheroine@gmail.com>
2021-12-14 14:45:45 +0100

    Ruby bool output example modified and its file renamed [#types]

--- ruby/types/type_bool/bool_definition_and_output.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby2.7
-
-a = true; b = false
-
-puts "a = #{a} (class: #{a.class})"
-puts "b = #{b} (class: #{b.class})"
---
+++ ruby/types/type_bool/bool_output.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby2.7
+
+a = true; b = false
+
+puts "a = #{a} (#{a.class})"
+puts "b = #{b} (#{b.class})"
