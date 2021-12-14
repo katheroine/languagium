0ee6810ac88c64047448f5058c6d347f06aaba9b
katheroine <katheroine@gmail.com>
2021-12-15 00:00:57 +0100

    Python infinity example modified [#types]

--- python/types/type_floating_point/infinity.py
+++ python/types/type_floating_point/infinity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 positive_infinity = float("inf")
-print("positive infinity: {}, type: {}".format(positive_infinity, type(positive_infinity)))
+print(f"positive_infinity = float(\"inf\") # {positive_infinity} ({type(positive_infinity)})")
 
 negative_infinity = float("-inf")
-print("negative infinity: {}, type: {}".format(negative_infinity, type(negative_infinity)))
+print(f"negative_infinity = float(\"-inf\") # {negative_infinity} ({type(negative_infinity)})")
