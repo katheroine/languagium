1abe2eb4d03d82029529d0fba70fda6f7841a1d6
katheroine <katheroine@gmail.com>
2021-12-14 21:23:21 +0100

    Perl integer literals oct and hex notations [#types]

---
+++ perl/types/integers/integer_literals_oct_and_hex_notation.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "15: " . 15 . "\n";
+print "017: " . 017 . "\n";
+print "0xf / 0Xf / 0xF / 0XF : " . 0xf . " / " . 0Xf . " / " . 0xF . " / " . 0XF . "\n\n";
