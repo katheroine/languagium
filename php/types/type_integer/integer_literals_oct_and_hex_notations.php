50f277e45f414493a7e0fa57f6f65e0be125c186
katheroine <katheroine@gmail.com>
2021-12-14 21:22:01 +0100

    PHP integer literals oct and hex notations example modified [#types]

--- php/types/type_integer/integer_literals_oct_and_hex_notations.php
+++ php/types/type_integer/integer_literals_oct_and_hex_notations.php
@@ -3,4 +3,4 @@
 
 print "15: " . 15 . "\n";
 print "017: " . 017 . "\n";
-print "0xf / 0Xf / 0xF / 0XF : " . 0xf . " / " . 0Xf . " / " . 0xF . " / " . 0XF . "\n";
+print "0xf / 0Xf / 0xF / 0XF : " . 0xf . " / " . 0Xf . " / " . 0xF . " / " . 0XF . "\n\n";
