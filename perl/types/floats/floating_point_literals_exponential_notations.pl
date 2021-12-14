904ee20f70274ba2be86d7675cec8cfc4143eac7
katheroine <katheroine@gmail.com>
2021-12-14 18:35:11 +0100

    Perl floating point literals exponential notation [#types]

---
+++ perl/types/floats/floating_point_literals_exponential_notations.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "5e3 / 5E3: " . 5e3 . " / " . 5E3 . "\n";
+print "5e+3 / 5E+3: " . 5e+3 . " / " . 5E+3 . "\n";
+print "5e-3 / 5E-3: " . 5e-3 . " / " . 5E-3 . "\n\n";
+
+print "1.5e3 / 1.5E3: " . 1.5e3 . " / " . 1.5E3 . "\n";
+print "1.5e+3 / 1.5E+3: " . 1.5e+3 . " / " . 1.5E+3 . "\n";
+print "1.5e-3 / 1.5E-3: " . 1.5e-3 . " / " . 1.5E-3 . "\n\n";
