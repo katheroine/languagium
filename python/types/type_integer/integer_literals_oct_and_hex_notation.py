ffaa311119b72354e5ebd9cc4ca28133862d1753
katheroine <katheroine@gmail.com>
2021-12-14 21:22:26 +0100

    Python integer literals oct and hex notations example modified [#types]

--- python/types/type_integer/integer_literals_oct_and_hex_notation.py
+++ python/types/type_integer/integer_literals_oct_and_hex_notation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-print("15: {}".format(15))
-print("0o17 / 0O17: {} / {}".format(0o17, 0O17));
-print("0xf / 0Xf / 0xF / 0XF : {} / {} / {} / {}".format(0xf, 0Xf, 0xF, 0XF));
+print(f"15: {15}")
+print(f"0o17 / 0O17: {0o17} / {0O17}");
+print(f"0xf / 0Xf / 0xF / 0XF : {0xf} / {0Xf} / {0xF} / {0XF}\n");
