abbbdfd5e88ac2c4c0699ac6f5ac1d9641334ce1
katheroine <katheroine@gmail.com>
2021-12-14 14:46:15 +0100

    Python bool output example modified and its file renamed [#types]

--- python/types/type_bool/bool_definition_and_output.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-a = True; b = False
-
-print("a = {}".format(a))
-print("b = {}".format(b))
---
+++ python/types/type_bool/bool_output.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a = True; b = False
+
+print(f"a = {a} ({type(a)})")
+print(f"b = {b} ({type(b)})")
