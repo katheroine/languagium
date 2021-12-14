e1347119f80512f858dc84818fbe010883f3a5d9
katheroine <katheroine@gmail.com>
2021-12-15 00:00:19 +0100

    JavaScript infinity example modified [#types]

--- javascript/types/floats/infinity.js
+++ javascript/types/floats/infinity.js
@@ -6,4 +6,7 @@ console.log("let positive_infinity = Infinity; // " + positive_infinity + " (" +
 let negative_infinity = -Infinity;
 console.log("let negative_infinity = -Infinity; // " + negative_infinity + " (" + typeof negative_infinity + ")");
 
-console.log("1/0 = " + 1/0);
+console.log();
+
+console.log("1/0: " + 1/0 + " (" + typeof 1/0 + ")");
+console.log("-1/0: " + -1/0 + " (" + typeof -1/0 + ")");
