7d6ad842ff19b027eb3139342eed7e263b0ff9fa
katheroine <katheroine@gmail.com>
2021-12-14 21:22:43 +0100

    Ruby integer literals oct and hex notations example modified [#types]

--- ruby/types/type_integer/integer_literals_bin_dec_oct_and_hex_notation.rb
+++ ruby/types/type_integer/integer_literals_bin_dec_oct_and_hex_notation.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby2.7
 
-puts "15: #{15}\n\n"
-
+puts "15: #{15}"
 puts "0b1111 / 0B1111: #{0b1111} / #{0B1111}"
 puts "0d15 / 0d15: #{0d15} / #{0d15}"
 puts "017: #{017}"
-puts "0xf / 0Xf / 0xF / 0XF : #{0xf} / #{0Xf} / #{0xF} / #{0XF}"
+puts "0xf / 0Xf / 0xF / 0XF : #{0xf} / #{0Xf} / #{0xF} / #{0XF}\n\n"
