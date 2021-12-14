40e8c7d832ffadfaee8233b441a4fddf8ea80a32
katheroine <katheroine@gmail.com>
2021-12-14 14:49:55 +0100

    PHP bool definition example modified [#types]

--- php/types/type_bool/bool_definition.php
+++ php/types/type_bool/bool_definition.php
@@ -3,26 +3,26 @@
 
 $a = true; $b = false;
 
-echo "\$a = true; // {$a} (" . {gettype($a)} . ")\n";
-echo "\$b = false; // {$b} (" . {gettype($b)} . ")\n";
+echo "\$a = true; // {$a} (" . gettype($a) . ")\n";
+echo "\$b = false; // {$b} (" . gettype($b) . ")\n";
 
 echo "\n";
 
 $a = True; $b = False;
 
-echo "\$a = True; // {$a} (" . {gettype($a)} . ")\n";
-echo "\$b = False; // {$b} (" . {gettype($b)} . ")\n";
+echo "\$a = True; // {$a} (" . gettype($a) . ")\n";
+echo "\$b = False; // {$b} (" . gettype($b) . ")\n";
 
 echo "\n";
 
 $a = TRUE; $b = FALSE;
 
-echo "\$a = TRUE; // {$a} (" . {gettype($a)} . ")\n";
-echo "\$b = FALSE; // {$b} (" . {gettype($b)} . ")\n";
+echo "\$a = TRUE; // {$a} (" . gettype($a) . ")\n";
+echo "\$b = FALSE; // {$b} (" . gettype($b) . ")\n";
 
 echo "\n";
 
 $a = (bool) 1; $b = (bool) 0;
 
-echo "\$a = (bool) 1; // {$a} (" . {gettype($a)} . ")\n";
-echo "\$b = (bool) 0; // {$b} (" . {gettype($b)} . ")\n";
+echo "\$a = (bool) 1; // {$a} (" . gettype($a) . ")\n";
+echo "\$b = (bool) 0; // {$b} (" . gettype($b) . ")\n";
