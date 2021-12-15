0ad3eafc284400d69cd5fde329621fb8664fc04a
katheroine <katheroine@gmail.com>
2021-12-15 17:26:31 +0100

    PHP floating point values limits example modified [#types]

--- php/types/type_floating_point/floating_point_values_limits.php
+++ php/types/type_floating_point/floating_point_values_limits.php
@@ -1,21 +1,11 @@
 #!/usr/bin/php8.0
 <?php
 
-print "The largest floating point supported: " . PHP_FLOAT_MAX . "\n";
-print "The smallest positive floating point supported: " . PHP_FLOAT_MIN . "\n";
-print "The smallest negative floating point supported: " . -PHP_FLOAT_MAX . "\n";
-print "The number of decimal digits that can be rounded into a float and back without precision loss: " . PHP_FLOAT_DIG . "\n";
-print "The smallest representable positive number x, so that x + 1.0 != 1.0: " . PHP_FLOAT_EPSILON . "\n";
-
-print "\n";
-
-$f = 10.5;
-print "{$f}: " . gettype($f) . "\n";
-$f = PHP_FLOAT_MIN;
-print "{$f} (minimal floating point): " . gettype($f) . "\n";
-$f = PHP_FLOAT_MIN - 0.1;
-print "{$f} (trying to make smaller than minimal floating point): " . gettype($f) . "\n";
-$f = PHP_FLOAT_MAX;
-print "{$f} (maximal floating point): " . gettype($f) . "\n";
+print "The largest (positive) floating point supported: PHP_FLOAT_MAX = " . PHP_FLOAT_MAX . " (" . gettype(PHP_FLOAT_MAX) . ")\n";
 $f = PHP_FLOAT_MAX + 0.1;
-print "{$f} (trying to make bigger than maximal floating point): " . gettype($f) . "\n";
+print "Trying to make bigger than maximal floating point: PHP_FLOAT_MAX + 0.1 = $f (" . gettype($f) . ")\n\n";
+print "The smallest (positive) floating point supported: PHP_FLOAT_MIN = " . PHP_FLOAT_MIN . " (" . gettype(PHP_FLOAT_MIN) . ")\n";
+$f = PHP_FLOAT_MIN - 0.1;
+print "Trying to make smaller than minimal floating point: PHP_FLOAT_MIN - 0.1 = $f (" . gettype($f) . ")\n\n";
+print "The number of decimal digits that can be rounded into a float and back without precision loss: PHP_FLOAT_DIG = " . PHP_FLOAT_DIG . " (" . gettype(PHP_FLOAT_DIG) . ")\n\n";
+print "The smallest representable positive number x, so that x + 1.0 != 1.0: PHP_FLOAT_EPSILON = " . PHP_FLOAT_EPSILON . " (" . gettype(PHP_FLOAT_EPSILON) . ")\n\n";
