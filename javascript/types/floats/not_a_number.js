67ff74dbd77ce0ed97875e6418d7587a41d652d9
katheroine <katheroine@gmail.com>
2021-12-15 11:47:33 +0100

    JavaScript not a number example modified [#types]

--- javascript/types/floats/not_a_number.js
+++ javascript/types/floats/not_a_number.js
@@ -3,4 +3,7 @@
 let not_a_number = NaN;
 console.log("let not_a_number = NaN; // " + not_a_number + " (" + typeof not_a_number + ")");
 
-console.log("0/0 = " + 0/0);
+console.log();
+
+console.log("0/0: " + 0/0 + " (" + typeof 0/0 + ")");
+console.log("-0/0: " + -0/0 + " (" + typeof -0/0 + ")");
