4a230c64561dfc1f29dd2e46ff9d193f4ffa08c0
katheroine <katheroine@gmail.com>
2021-12-15 13:51:02 +0100

    PHP checking if is floating point example modified [#types]

--- php/types/type_floating_point/checking_if_is_floating_point.php
+++ php/types/type_floating_point/checking_if_is_floating_point.php
@@ -1,6 +1,33 @@
 #!/usr/bin/php8.0
 <?php
 
-$v = 10.1;
-print "{$v}: " . is_float($v) . "\n";
-print "{$v}: " . is_double($v) . "\n"; // alias of is_float()
+print "is_float(0): " . is_float(0) . "\n";
+print "is_float(1): " . is_float(1) . "\n";
+print "is_float(-1): " . is_float(-1) . "\n";
+print "is_float(1.0): " . is_float(1.0) . "\n";
+print "is_float(-1.0): " . is_float(-1.0) . "\n";
+print "is_float(1.1): " . is_float(1.1) . "\n";
+print "is_float(-1.1): " . is_float(-1.1) . "\n";
+print "is_float(1.9e411): " . is_float(1.9e411) . " // infinity\n";
+print "is_float(-1.9e411): " . is_float(-1.9e411) . " // -infinity\n";
+print "is_float(acos(2)): " . is_float(acos(2)) . " // not-a-number\n";
+print "is_float(null): " . is_float(null) . "\n";
+print "is_float('1.1'): " . is_float('1.1') . "\n";
+
+print("\n");
+
+// is_float alias:
+print "is_double(0): " . is_double(0) . "\n";
+print "is_double(1): " . is_double(1) . "\n";
+print "is_double(-1): " . is_double(-1) . "\n";
+print "is_double(1.0): " . is_double(1.0) . "\n";
+print "is_double(-1.0): " . is_double(-1.0) . "\n";
+print "is_double(1.1): " . is_double(1.1) . "\n";
+print "is_double(-1.1): " . is_double(-1.1) . "\n";
+print "is_double(1.9e411): " . is_double(1.9e411) . " // infinity\n";
+print "is_double(-1.9e411): " . is_double(-1.9e411) . " // -infinity\n";
+print "is_double(acos(2)): " . is_double(acos(2)) . " // not-a-number\n";
+print "is_double(null): " . is_double(null) . "\n";
+print "is_double('1.1'): " . is_double('1.1') . "\n";
+
+print("\n");
