9ec5c2f2fe5125a3774083e0bfc72e7ab351746b
katheroine <katheroine@gmail.com>
2021-12-16 11:30:32 +0100

    Python types example modified [#types]

--- python/types/types.py
+++ python/types/types.py
@@ -3,11 +3,14 @@
 b = True
 i = 5
 f = 2.4
+x = 1+2j
 s = "hello"
+r = range(3, 7, 2)
 l = [3, 5, 7]
 t = (3, 5, 7)
-z = {3, 4, 5}
-d = {
+e = {3, 5, 7}
+z = frozenset({3, 5, 7})
+g = {
   2: "Hello, there!",
   'color': 'orange',
   3.14: 'PI',
@@ -17,9 +20,12 @@ u = lambda number: number * 3
 print(f"b = True # boolean: {b} ({type(b)})\n")
 print(f"i = 5 # integer: {i} ({type(i)})\n")
 print(f"f = 2.4 # floating point: {f} ({type(f)})\n")
+print(f"x = 1+2j # complex: {x} ({type(x)})\n")
 print(f"s = \"hello\" # string: {s} ({type(s)})\n")
+print(f"r = range(3, 7, 2) # range: {r} ({type(r)})\n")
 print(f"l = [3, 5, 7] # list: {l} ({type(l)})\n")
 print(f"t = (3, 5, 7) # tuple: {t} ({type(t)})\n")
-print(f"z = {{3, 4, 5}} # set: {z} ({type(z)})\n")
-print(f"d = {{\n  2: \"Hello, there!\",\n  'color': 'orange',\n  3.14: 'PI',\n}}\n# dictionary: {d} ({type(d)})\n")
+print(f"e = {{3, 4, 5}} # set: {e} ({type(e)})\n")
+print(f"z = frozenset({{3, 4, 5}}) # frozen set: {z} ({type(z)})\n")
+print(f"g = {{\n  2: \"Hello, there!\",\n  'color': 'orange',\n  3.14: 'PI',\n}}\n# dictionary: {g} ({type(g)})\n")
 print(f"u = lambda number: number * 3 # function: {u} ({type(u)})\n")
