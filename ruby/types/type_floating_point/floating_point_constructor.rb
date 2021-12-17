8ca87ce1e34b22e1f40c2a506f2a7f962a4175b6
katheroine <katheroine@gmail.com>
2021-12-17 14:22:16 +0100

    Ruby floating point constructor example modified [#types]

--- ruby/types/type_floating_point/floating_point_constructor.rb
+++ ruby/types/type_floating_point/floating_point_constructor.rb
@@ -1,37 +1,37 @@
 #!/usr/bin/ruby2.7
 
 f = Float(0.0)
-puts "#{f}: #{f.class}"
+puts "f = Float(0.0) # #{f} (#{f.class})"
 
 f = Float(5.2)
-puts "#{f}: #{f.class}"
+puts "f = Float(5.2) # #{f} (#{f.class})"
 
 f = Float(-5.2)
-puts "#{f}: #{f.class}"
-
-f = Float("0.0")
-puts "#{f}: #{f.class}"
-
-f = Float("5.2")
-puts "#{f}: #{f.class}"
-
-f = Float("-5.2")
-puts "#{f}: #{f.class}"
+puts "f = Float(-5.2) # #{f} (#{f.class})"
 
 f = Float(0)
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(0) # #{f} (#{f.class})"
 
 f = Float(5)
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(5) # #{f} (#{f.class})"
 
 f = Float(-5)
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(-5) # #{f} (#{f.class})"
+
+f = Float("0.0")
+puts "f = Float(\"0.0\") # #{f} (#{f.class})"
+
+f = Float("5.2")
+puts "f = Float(\"5.2\") # #{f} (#{f.class})"
+
+f = Float("-5.2")
+puts "f = Float(\"-5.2\") # #{f} (#{f.class})"
 
 f = Float("0")
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(\"0\") # #{f} (#{f.class})"
 
 f = Float("5")
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(\"5\") # #{f} (#{f.class})"
 
 f = Float("-5")
-puts "#{f} (constructed from integer): #{f.class}"
+puts "f = Float(\"-5\") # #{f} (#{f.class})"
