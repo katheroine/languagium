92a4c11a192836122ebd4574dd51fbf2f11d94f4
katheroine <katheroine@gmail.com>
2021-12-17 14:59:42 +0100

    Python floating point constructor example modified [#types]

--- python/types/type_floating_point/floating_point_constructor.py
+++ python/types/type_floating_point/floating_point_constructor.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
 
 f = float(0.0)
-print("{}: {}".format(f, type(f)))
+print(f"f = float(0.0) # {f} ({type(f)})")
 
 f = float(5.2)
-print("{}: {}".format(f, type(f)))
+print(f"f = float(5.2) # {f} ({type(f)})")
 
 f = float(-5.2)
-print("{}: {}".format(f, type(f)))
-
-f = float("0.0")
-print("{}: {}".format(f, type(f)))
-
-f = float("5.2")
-print("{}: {}".format(f, type(f)))
-
-f = float("-5.2")
-print("{}: {}".format(f, type(f)))
+print(f"f = float(-5.2) # {f} ({type(f)})")
 
 f = float(0)
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(0) # {f} ({type(f)})")
 
 f = float(5)
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(5) # {f} ({type(f)})")
 
 f = float(-5)
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(-5) # {f} ({type(f)})")
+
+f = float("0.0")
+print(f"f = float(\"0.0\") # {f} ({type(f)})")
+
+f = float("5.2")
+print(f"f = float(\"5.2\") # {f} ({type(f)})")
+
+f = float("-5.2")
+print(f"f = float(\"-5.2\") # {f} ({type(f)})")
 
 f = float("0")
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(\"0\") # {f} ({type(f)})")
 
 f = float("5")
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(\"5\") # {f} ({type(f)})")
 
 f = float("-5")
-print("{} (constructed from integer): {}".format(f, type(f)))
+print(f"f = float(\"-5\") # {f} ({type(f)})")
