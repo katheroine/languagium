91488333d5f39c6e3e4850c8ff8cd389b94969e9
katheroine <katheroine@gmail.com>
2021-12-17 13:46:43 +0100

    Ruby integer constructor example modified [#types]

--- ruby/types/type_integer/integer_constructor.rb
+++ ruby/types/type_integer/integer_constructor.rb
@@ -1,25 +1,25 @@
 #!/usr/bin/ruby2.7
 
 i = Integer(0)
-puts "#{i}: #{i.class}"
+puts "i = Integer(0) # #{i} (#{i.class})"
 
 i = Integer(5)
-puts "#{i}: #{i.class}"
+puts "i = Integer(5) # #{i} (#{i.class})"
 
 i = Integer(-5)
-puts "#{i}: #{i.class}"
-
-i = Integer("0")
-puts "#{i}: #{i.class}"
-
-i = Integer("5")
-puts "#{i}: #{i.class}"
-
-i = Integer("-5")
-puts "#{i}: #{i.class}"
+puts "i = Integer(-5) # #{i} (#{i.class})"
 
 i = Integer(5.2)
-puts "#{i} (constructed from from floating point): #{i.class}"
+puts "i = Integer(5.2) # #{i} (#{i.class})"
 
-#i = Integer("5.2") # error
-#puts "#{i} (constructed from from floating point): #{i.class}"
+i = Integer("0")
+puts "i = Integer(\"0\") # #{i} (#{i.class})"
+
+i = Integer("5")
+puts "i = Integer(\"5\") # #{i} (#{i.class})"
+
+i = Integer("-5")
+puts "i = Integer(\"-5\") # #{i} (#{i.class})"
+
+# i = Integer("5.2") # invalid value for Integer(): "5.2" (ArgumentError)
+# puts "i = Integer(\"5.2\") # #{i} (#{i.class})"
