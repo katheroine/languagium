ea1889318d92dd45b5b9c8a8abff28a9800c30e5
katheroine <katheroine@gmail.com>
2021-12-20 11:58:38 +0100

    Python string length [#types]

---
+++ python/types/strings/string_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+word = "hello"
+print(f"word = {word}")
+print(f"length: {len(word)}\n")
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+print(f"cite = {cite}")
+print(f"length: {len(cite)}")
+print(f"words quantity: {len(cite.split())}\n")
