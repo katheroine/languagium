7afa034b3429abbd7820df90f96b0aaaa1e3cf9f
katheroine <katheroine@gmail.com>
2021-12-20 11:36:15 +0100

    PHP string length [#types]

---
+++ php/types/strings/string_length.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php8.0
+<?php
+
+$word = "hello";
+print("word = " . $word . "\n");
+print("length: " . strlen($word) . "\n\n");
+
+$cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+print("cite = " . $cite . "\n");
+print("length: " . strlen($cite) . "\n");
+print("words quantity: " . str_word_count($cite) . "\n\n");
