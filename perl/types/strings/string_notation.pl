d0f37eb95e3b579466ef1ba8414a6c4e5443a6d6
katheroine <katheroine@gmail.com>
2021-12-20 14:14:43 +0100

    Perl string notation example completed [#types]

--- perl/types/strings/string_notation.pl
+++ perl/types/strings/string_notation.pl
@@ -8,3 +8,12 @@ print("\"hello\": " . $s . "\n");
 
 $s = 'hello';
 print("'hello': " . $s . "\n");
+
+$s = q\hello\;
+print("q\\hello\\: " . $s . "\n");
+
+$s = qq\hello\;
+print("qq\\hello\\: " . $s . "\n");
+
+$s = q|hello|;
+print("q|hello|: " . $s . "\n");
