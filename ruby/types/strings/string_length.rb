578292d052d7b10fcca9b8b4d26e606b19fe94de
katheroine <katheroine@gmail.com>
2021-12-20 11:36:30 +0100

    Ruby string length [#types]

---
+++ ruby/types/strings/string_length.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby2.7
+
+word = "hello"
+puts("word = #{word}")
+puts("length: #{word.length}\n\n")
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+puts("cite = #{cite}")
+puts("length: #{cite.length}")
+puts("words quantity: #{cite.split(" ").length}\n\n")
