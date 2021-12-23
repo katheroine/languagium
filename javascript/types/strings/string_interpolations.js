f02438fc582b29f8c2871403b8551b83e2c65f92
katheroine <katheroine@gmail.com>
2021-12-23 13:56:52 +0100

    JavaScript string interpolations example modified [#types]

--- javascript/types/strings/string_interpolations.js
+++ javascript/types/strings/string_interpolations.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-let i = 1, f = 2.3, c = "apple";
+let i = 1, f = 2.3, s = "apple";
 
-console.log(`i = ${i}, f = ${f}, c = ${c}\n`);
+console.log(`i = ${i}, f = ${f}, s = ${s}\n`);
 
 let a = [
   "orange",
@@ -13,9 +13,9 @@ let a = [
 console.log(`a[0] = ${a[0]}, a[1] = ${a[1]}, a[2] = ${a[2]}\n`);
 
 let g = {
-  "text_0": "Stat rosa pristina nomine, nomina nuda tenemus.",
-  "text_1": "Omnis mundi creatura quasi liber et pictura nobis est in speculum.",
-  "text_2": "Videmus nunc per speculum et in aenigmate.",
+  'text_0': "Stat rosa pristina nomine, nomina nuda tenemus.",
+  'text_1': "Omnis mundi creatura quasi liber et pictura nobis est in speculum.",
+  'text_2': "Videmus nunc per speculum et in aenigmate.",
 };
 
 console.log(`g['text_0']: ${g['text_0']}`);
