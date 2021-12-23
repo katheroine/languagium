674b0b6ff14c4ef02c76e32cc51bb16d6ec0ad9b
katheroine <katheroine@gmail.com>
2021-12-23 14:00:46 +0100

    Python string interpolations example modified [#types]

--- python/types/strings/string_interpolations.py
+++ python/types/strings/string_interpolations.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
 
-a = 1; b = 2; c = 3
+i = 1; f = 2.3; s = "apple"
 
-print("a = %i"%a)
-print(f"b = {b}")
-print("c = {}".format(c))
-print("c = {0}".format(c))
-print("c = {c_value}".format(c_value = c))
+print("i = %i, f = %f, s = %s"%(i, f, s))
+print(f"i = {i}, f = {f}, s = {s}")
+print("i = {}, f = {}, s = {}".format(i, f, s))
+print("i = {0}, f = {1}, s = {2}".format(i, f, s))
+print("i = {integer_value}, f = {floating_point_value}, s = {string_value}".format(integer_value = i, floating_point_value = f, string_value = s))
+print()
 
-print("a = %i, b = %i, c = %i"%(a, b, c))
-print(f"a = {a}, b = {b}, c = {c}")
-print("a = {}, b = {}, c = {}".format(a, b, c))
-print("a = {0}, b = {1}, c = {2}".format(a, b, c))
-print("a = {a_value}, b = {b_value}, c = {c_value}".format(a_value = a, b_value = b, c_value = c))
+a = [
+  "orange",
+  "blue",
+  "green",
+]
 
-print("\n")
+print("a[0] = %s, a[1] = %s, a[2] = %s"%(a[0], a[1], a[2]))
+print(f"a[0] = {a[0]}, a[1] = {a[1]}, a[2] = {a[2]}")
+print("a[0] = {}, a[1] = {}, a[2] = {}".format(a[0], a[1], a[2]))
+print("a[0] = {0}, a[1] = {1}, a[2] = {2}".format(a[0], a[1], a[2]))
+print("a[0] = {element_0}, a[1] = {element_1}, a[2] = {element_2}".format(element_0 = a[0], element_1 = a[1], element_2 = a[2]))
+print()
 
-f = 235.846279302548003
+g = {
+  'text_0': "Stat rosa pristina nomine, nomina nuda tenemus.",
+  'text_1': "Omnis mundi creatura quasi liber et pictura nobis est in speculum.",
+  'text_2': "Videmus nunc per speculum et in aenigmate.",
+}
 
-print("f = %f"%f)
-print("f = %.3f"%f)
-print("f = %5.2f"%f)
+print("g['text_0'] = %s"%g['text_0'])
+print("g['text_1'] = %s"%g['text_1'])
+print("g['text_2'] = %s"%g['text_2'])
 
-print("\n")
+print(f"g['text_0'] = {g['text_0']}")
+print(f"g['text_1'] = {g['text_1']}")
+print(f"g['text_2'] = {g['text_2']}")
 
-print('%d %s cost $%.2f' % (6, 'bananas', 1.74))
-# source: https://realpython.com/python-formatted-output/#the-string-format-method-arguments
+print("g['text_0'] = {}".format(g['text_0']))
+print("g['text_1'] = {}".format(g['text_1']))
+print("g['text_2'] = {}".format(g['text_2']))
+
+print("g['text_0'] = {0}".format(g['text_0']))
+print("g['text_1'] = {0}".format(g['text_1']))
+print("g['text_2'] = {0}".format(g['text_2']))
