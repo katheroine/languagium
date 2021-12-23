a38ad34bef76c48c7af1d459586f366fb8baa544
katheroine <katheroine@gmail.com>
2021-12-23 11:33:49 +0100

    Ruby string interpolations example modified [#types]

--- ruby/types/strings/string_interpolations.rb
+++ ruby/types/strings/string_interpolations.rb
@@ -1,5 +1,24 @@
 #!/usr/bin/ruby2.7
 
-a = 1; b = 2; c = 3
+i = 1; f = 2.3; c = "apple"
 
-puts("a = #{a}, b = #{b}, c = #{c}")
+puts("i = #{i}, f = #{f}, c = #{c}\n\n")
+
+a = [
+  "orange",
+  "blue",
+  "green",
+]
+
+puts("a[0] = #{a[0]}, a[1] = #{a[1]}, a[2] = #{a[2]}\n\n")
+
+g = {
+  "text_0" => "Stat rosa pristina nomine, nomina nuda tenemus.",
+  "text_1" => "Omnis mundi creatura quasi liber et pictura nobis est in speculum.",
+  "text_2" => "Videmus nunc per speculum et in aenigmate.",
+}
+
+puts("g['text_0']: #{g['text_0']}")
+puts("g['text_1']: #{g['text_1']}")
+puts("g['text_2']: #{g['text_2']}")
+puts()
