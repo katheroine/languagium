3ab363e543886f08833e4d95278943f471f56f67
katheroine <katheroine@gmail.com>
2021-12-25 14:22:46 +0100

    JavaScript string cases [#types]

---
+++ javascript/types/strings/string_cases.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+let cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+console.log(`cite = ${cite}\n`);
+
+let upper_case_cite = cite.toUpperCase();
+console.log(`upper case: ${upper_case_cite}`);
+
+let lower_case_cite = cite.toLowerCase();
+console.log(`lower case: ${lower_case_cite}`);
