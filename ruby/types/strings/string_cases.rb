1ed57e78cd2a257f43b360faa411ae742a288472
katheroine <katheroine@gmail.com>
2021-12-25 16:16:44 +0100

    Ruby string cases [#types]

---
+++ ruby/types/strings/string_cases.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby2.7
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+puts("cite = #{cite}\n\n")
+
+upper_case_cite = cite.upcase()
+puts("upper case: #{upper_case_cite}")
+
+lower_case_cite = cite.downcase()
+puts("lower case: #{lower_case_cite}")
