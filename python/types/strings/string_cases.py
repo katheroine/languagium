5d4330e4bae11cab657f8c3db8ff978aff2db503
katheroine <katheroine@gmail.com>
2021-12-25 16:04:40 +0100

    Python string cases [#types]

---
+++ python/types/strings/string_cases.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+print(f"cite = {cite}\n")
+
+upper_case_cite = cite.upper()
+print(f"upper case: {upper_case_cite}")
+
+lower_case_cite = cite.lower()
+print(f"lower case: {lower_case_cite}")
