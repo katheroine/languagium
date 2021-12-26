e56443137fbff33a754c18cd4f55eb81792397f6
katheroine <katheroine@gmail.com>
2021-12-26 22:29:05 +0100

    Ruby string trimming [#types]

---
+++ ruby/types/strings/string_trimming.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby2.7
+
+sententia = "   Sapere aude.   "
+puts("sententia = <#{sententia}>\n\n")
+
+trimmed_sententia = sententia.strip
+
+puts("trimmed sententia = <#{trimmed_sententia}>\n\n")
