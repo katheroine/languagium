a749cb480c45d5882f4b2a8504fddbb2cfef9697
katheroine <katheroine@gmail.com>
2021-12-26 15:11:13 +0100

    Ruby modification of the substring [#types]

---
+++ ruby/types/strings/substring_modification.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby2.7
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+puts("cite = #{cite}\n\n")
+
+modified_cite = cite.sub("rosa", "odonata")
+
+puts("modified cite = #{modified_cite}\n\n")
