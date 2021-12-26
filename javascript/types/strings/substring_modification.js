5e7dfbeaa3ccba83b114a8f12855e2831471f8cf
katheroine <katheroine@gmail.com>
2021-12-26 14:48:22 +0100

    JavaScript modification of the substring [#types]

---
+++ javascript/types/strings/substring_modification.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+let cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+console.log(`cite = ${cite}\n`);
+
+let modified_cite = cite.replace("rosa", "odonata");
+
+console.log(`modified cite = ${modified_cite}\n`);
