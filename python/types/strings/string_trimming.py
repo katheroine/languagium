2be64f5c519871a60f0bcf8408bb2d9f9f88cf3b
katheroine <katheroine@gmail.com>
2021-12-26 21:44:29 +0100

    Python string trimming [#types]

---
+++ python/types/strings/string_trimming.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+sententia = "   Sapere aude.   "
+print(f"sententia = <{sententia}>\n")
+
+trimmed_sententia = sententia.strip()
+
+print(f"trimmed sententia = <{trimmed_sententia}>\n")
