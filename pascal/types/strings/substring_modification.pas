8c0d01d3aa86fe7faf24ea4a008deaad51827877
katheroine <katheroine@gmail.com>
2021-12-26 14:47:43 +0100

    Pascal modification of the substring [#types]

---
+++ pascal/types/strings/substring_modification.pas
@@ -0,0 +1,18 @@
+program SearchSubstring;
+
+uses strutils;
+
+var
+  cite: string = 'Stat rosa pristina nomine, nomina nuda tenemus.';
+  modified_cite: string;
+
+begin
+  writeln('cite = ', cite);
+  writeln();
+
+  modified_cite := replacestr(cite, 'rosa', 'odonata');
+
+  writeln('modified cite = ', modified_cite);
+  writeln();
+
+end.
