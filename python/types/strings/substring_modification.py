13e0d09399c6e6828a229181ef0fccacb1bbbec8
katheroine <katheroine@gmail.com>
2021-12-26 15:07:29 +0100

    Python modification of the substring [#types]

---
+++ python/types/strings/substring_modification.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+cite = "Stat rosa pristina nomine, nomina nuda tenemus."
+print(f"cite = {cite}\n")
+
+modified_cite = cite.replace("rosa", "odonata")
+
+print(f"modified cite = {modified_cite}\n")
