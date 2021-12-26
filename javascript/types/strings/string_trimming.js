3280287efb0af612e5097918a7b2011bd86226e9
katheroine <katheroine@gmail.com>
2021-12-26 19:10:50 +0100

    JavaScript string trimming [#types]

---
+++ javascript/types/strings/string_trimming.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+let sententia = "   Sapere aude.   ";
+console.log(`sententia = <${sententia}>\n`);
+
+let trimmed_sententia = sententia.trim();
+
+console.log(`trimmed sententia = <${trimmed_sententia}>\n`);
