1fb9e875d7c04741b6c72982f27afa577e66ef36
katheroine <katheroine@gmail.com>
2021-12-27 12:35:08 +0100

    Ruby string notation example completed [#types]

--- ruby/types/strings/string_notation.rb
+++ ruby/types/strings/string_notation.rb
@@ -5,3 +5,21 @@ puts("\"hello\": #{s} (#{s.class})")
 
 s = 'hello'
 puts("'hello': #{s} (#{s.class})")
+
+s = %q(hello)
+puts("%q(hello): #{s} (#{s.class})")
+
+s = %Q(hello)
+puts("%Q(hello): #{s} (#{s.class})")
+
+s = %q[hello]
+puts("%q[hello]: #{s} (#{s.class})")
+
+s = %Q[hello]
+puts("%Q[hello]: #{s} (#{s.class})")
+
+s = %q|hello|
+puts("%q|hello|: #{s} (#{s.class})")
+
+s = %Q|hello|
+puts("%Q|hello|: #{s} (#{s.class})")
