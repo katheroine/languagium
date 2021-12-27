477bb34f4fbc4805742a67c4dbe52e2b8e7dd6b5
katheroine <katheroine@gmail.com>
2021-12-27 12:37:56 +0100

    Perl strings [#types]

---
+++ perl/types/strings/strings.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $s = "hello";
+print("string \"hello\": $s (", (ref($s) || "scalar"), ")\n");
+
+$s = 'hello';
+print("string 'hello': $s (", (ref($s) || "scalar"), ")\n");
+
+print("length: ", length($s), "\n\n");
+
+my $cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+print("cite: $cite\n");
+
+my $words_quantity = split(/\s+/, $cite);
+print("words quantity: ", $words_quantity, "\n\n");
+
+my $s1 = "abc";
+my $s2 = "def";
+my $s3 = $s1 . $s2;
+print("$s1 . $s2 = $s3\n\n");
