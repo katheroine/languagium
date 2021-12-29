8f3ea2c4ccc3f3538ca4b6f8a41080728febabb0
katheroine <katheroine@gmail.com>
2021-12-29 16:21:38 +0100

    PHP do-while loop example splited into smaller parts [#control_flow]

--- php/control_flow/do_while_loop/do_while_loop.php
+++ php/control_flow/do_while_loop/do_while_loop.php
@@ -9,7 +9,3 @@ do {
 } while ($number < 10);
 
 echo "\n";
-
-$number = 0;
-
-do { echo("{$number}...\n"); ++$number; } while ($number < 10);
---
+++ php/control_flow/do_while_loop/do_while_loop_formatting.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+$number = 0;
+
+do {
+  echo("{$number}...\n");
+  ++$number;
+} while ($number < 10);
+
+echo "\n";
+
+$number = 0;
+
+do { echo("{$number}...\n"); ++$number; } while ($number < 10);
+
+echo "\n";
