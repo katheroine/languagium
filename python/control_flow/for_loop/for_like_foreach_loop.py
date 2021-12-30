761e09a3f47871fd14f55c972a2f379e199fab02
katheroine <katheroine@gmail.com>
2021-12-30 13:15:35 +0100

    Python for loop examples splited into smaller parts [#control_flow]

--- python/control_flow/for_loop/for_like_foreach_loop.py
+++ python/control_flow/for_loop/for_like_foreach_loop.py
@@ -5,6 +5,4 @@ a = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 for i in a:
     print(f'{i}...')
 
-print();
-
-for i in a: print(f'{i}...')
+print()
--- python/control_flow/for_loop/for_loop.py
+++ python/control_flow/for_loop/for_loop.py
@@ -3,6 +3,4 @@
 for i in range(10):
     print(f'{i}...')
 
-print();
-
-for i in range(10): print(f'{i}...')
+print()
