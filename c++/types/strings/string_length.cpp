6bf35db3e43a13d5cb7ff79ee0ba9b1ebefe2088
katheroine <katheroine@gmail.com>
2021-12-31 19:38:02 +0100

    C++ string length [#types]

---
+++ c++/types/strings/string_length.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string word = "hello";
+  std::cout << "word = " << word << "\n";
+  std::cout << "length: " << word.length() << "\n\n";
+
+  std::string cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+  std::cout << "cite = " << cite << "\n";
+  std::cout << "length: " << cite.length() << "\n\n";
+}
