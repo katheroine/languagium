eaefaf459b2f37843b11c82161f61600b2ffc3fa
katheroine <katheroine@gmail.com>
2022-01-03 15:14:30 +0100

    Ruby complexes example modified [#types]

--- ruby/types/complexex/complexes.rb
+++ ruby/types/complexex/complexes.rb
@@ -4,16 +4,20 @@ x = -1 + 2i
 puts("x: #{x} (#{x.class})")
 puts("real: #{x.real}")
 puts("imaginary: #{x.imaginary}")
-puts("phase angle: #{x.phase}")
-puts("magnitude: #{x.magnitude}\n\n")
+puts("magnitude: #{x.magnitude} = #{x.abs}")
+puts("phase angle: #{x.phase} = #{x.angle}")
+puts("magnitude and phase angle: #{x.polar}")
+puts("complex conjugate: #{x.conjugate}")
+puts()
 
 magnitude = 2.23606
 phase_angle = 2.03444
 x = Complex.polar(magnitude, phase_angle)
-puts("x (constructed from magnitude and angle): #{x}\n\n")
+puts("x (constructed from magnitude and angle): #{x}")
+puts()
 
 x1 = 2 + 3i
-x2 = 4 + 1i
+x2 = 4 - 1i
 x3 = x1 + x2
 puts("#{x1} + #{x2} = #{x3}")
 x3 = x1 - x2
@@ -21,4 +25,7 @@ puts("#{x1} - #{x2} = #{x3}")
 x3 = x1 * x2
 puts("#{x1} * #{x2} = #{x3}")
 x3 = x1 / x2
-puts("#{x1} / #{x2} = #{x3}\n\n")
+puts("#{x1} / #{x2} = #{x3}")
+x3 = x1**2
+puts("#{x1}^2 = #{x3}")
+puts()
