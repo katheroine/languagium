af360346bc6528c793a20580d500c73615136c5e
katheroine <katheroine@gmail.com>
2022-01-03 12:51:12 +0100

    Pascal complexes example modified [#types]

--- pascal/types/complexes/complexes.pas
+++ pascal/types/complexes/complexes.pas
@@ -14,8 +14,7 @@ begin
   writeln();
 
   x1 := 2 + 3*i;
-  x2 := 4 + 1*i;
-
+  x2 := 4 - 1*i;
   x3 := x1 + x2;
   writeln(Format('(%.2f + %.2fi) + (%.2f + %.2fi) = (%.2f + %.2fi)', [x1.re, x1.im, x2.re, x2.im, x3.re, x3.im]));
   x3 := x1 - x2;
