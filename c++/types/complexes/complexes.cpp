31780b19a95afa348ae1bfbc662cbfbef12cd2b3
katheroine <katheroine@gmail.com>
2022-01-03 11:47:02 +0100

    C++ complexes example modified [#types]

--- c++/types/complexes/complexes.cpp
+++ c++/types/complexes/complexes.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include <complex>
+#include <cmath>
 #include <typeinfo>
 
 using namespace std::complex_literals;
@@ -8,20 +10,24 @@ int main()
 {
   std::complex<double> x = -1. + 2i;
   std::cout << "x: " << x << " (" << typeid(x).name() << ")\n";
-  std::cout << "real: " << x.real() << " = " << real(x) << "\n";
-  std::cout << "imaginary: " << x.imag() << " = " << imag(x) << "\n";
-  std::cout << "squared magnitude: " << norm(x) << "\n";
-  std::cout << "phase angle: " << arg(x) << "\n";
-  std::cout << "complex conjugate: " << conj(x) << "\n\n";
+  std::cout << "real: " << x.real() << " = " << real(x) << std::endl;
+  std::cout << "imaginary: " << x.imag() << " = " << imag(x) << std::endl;
+  std::cout << "magnitude: " << abs(x) << std::endl;
+  std::cout << "phase angle: " << arg(x) << std::endl;
+  std::cout << "squared magnitude: " << norm(x) << std::endl;
+  std::cout << "complex conjugate: " << conj(x) << std::endl;
+  std::cout << std::endl;
 
-  double squared_magnitude = 5;
-  double phase_angle = 2.03444;
-  x = std::polar(sqrt(squared_magnitude), phase_angle);
-  std::cout << "x (constructed from magnitude and angle): " << x << "\n\n";
+  double magnitude = 2.236070;
+  double phase_angle = 2.034440;
+  x = std::polar(magnitude, phase_angle);
+  std::cout << "x (constructed from magnitude and angle): " << x << std::endl;
+  std::cout << std::endl;
 
   std::complex<double> x1 = 2. + 3i;
-  std::complex<double> x2 = 4. + 1i;
+  std::complex<double> x2 = 4. - 1i;
   std::complex<double> x3;
+  std::cout << std::fixed << std::setprecision(2);
   x3 = x1 + x2;
   std::cout << x1 << " + " << x2 << " = " << x3 << std::endl;
   x3 = x1 - x2;
@@ -30,5 +36,9 @@ int main()
   std::cout << x1 << " * " << x2 << " = " << x3 << std::endl;
   x3 = x1 / x2;
   std::cout << x1 << " / " << x2 << " = " << x3 << std::endl;
+  x3 = pow(x1, 2);
+  std::cout << x1 << "^2" << " = " << x3 << std::endl;
+  x3 = sqrt(x2);
+  std::cout << "√" << x2 << " = " << x3 << std::endl;
   std::cout << std::endl;
 }
