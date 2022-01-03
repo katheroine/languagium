5db1ab666566255a01f5da2a796445bf57bf79cb
katheroine <katheroine@gmail.com>
2022-01-03 13:57:06 +0100

    Python complexes example modified [#types]

--- python/types/complexes/complexes.py
+++ python/types/complexes/complexes.py
@@ -7,15 +7,17 @@ print(f"x: {x} ({type(x)})")
 print(f"real: {x.real}")
 print(f"imaginary: {x.imag}")
 print(f"phase angle: {cmath.phase(x)}")
-print(f"magnitude and phase angle: {cmath.polar(x)}\n")
+print(f"magnitude and phase angle: {cmath.polar(x)}")
+print()
 
-magnitude = 2.23606
-phase_angle = 2.03444
+magnitude = 2.236060
+phase_angle = 2.034440
 x = cmath.rect(magnitude, phase_angle)
-print(f"x (constructed from magnitude and angle): {x}\n")
+print(f"x (constructed from magnitude and angle): {x}")
+print()
 
 x1 = 2 + 3j
-x2 = 4 + 1j
+x2 = 4 - 1j
 x3 = x1 + x2
 print(f"{x1} + {x2} = {x3}")
 x3 = x1 - x2
@@ -23,4 +25,9 @@ print(f"{x1} + {x2} = {x3}")
 x3 = x1 * x2
 print(f"{x1} + {x2} = {x3}")
 x3 = x1 / x2
-print(f"{x1} + {x2} = {x3}\n")
+print(f"{x1} + {x2} = {x3}")
+x3 = x1**2
+print(f"{x1}^2 = {x3}")
+x3 = cmath.sqrt(x2)
+print(f"√{x2} = {x3}")
+print()
