80bcedde65a77b8620ffdc8b2fa0ad2586550f05
katheroine <katheroine@gmail.com>
2022-01-04 14:40:15 +0100

    Python ranges [#types]

---
+++ python/types/ranges/ranges.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+r = range(5, 8, 1)
+print(f"r = range (5, 8, 1) ({type(r)})")
+print("elements: ", end = '')
+for e in r:
+  print(e, " ", end = '')
+print()
+print(f"[1]: {r[1]}")
+print(f"[:1]: {r[:1]}")
+print(f"[1:2]: {r[1:2]}")
+print(f"index of element 6: {r.index(6)}")
+print()
+
+r = range(5, 10, 2)
+print(f"r = range (5, 10, 2) ({type(r)})")
+print("elements: ", end = '')
+for e in r:
+  print(e, " ", end = '')
+print()
+print(f"[1]: {r[1]}")
+print(f"[:1]: {r[:1]}")
+print(f"[1:2]: {r[1:2]}")
+print(f"index of element 7: {r.index(7)}")
+print()
