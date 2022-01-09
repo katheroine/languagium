060c01ade4acae6c4000432a659ab124d852a916
katheroine <katheroine@gmail.com>
2022-01-09 15:59:29 +0100

    Perl value of assignment expression (returned by the assignment operator) [#operators]

---
+++ perl/operators/operator_assignment_value.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $i, my $iv;
+$iv = ($i = 3);
+print("(\$i = 3) = ${iv}\n");
+
+my $f, my $fv;
+$fv = ($f = 2.5);
+print("(\$f = 2.5) = ${fv}\n");
