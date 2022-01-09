f1ed320ea645ebfe615f119b43da3f340d149787
katheroine <katheroine@gmail.com>
2022-01-09 13:31:49 +0100

    Python operator ternary substitute [#operators]

---
+++ python/operators/operator_ternary_substitute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+value = float(input("Give some value: "))
+
+state = "low" if (value < 10) else "high"
+
+print(f"Value is {state}. ", end = "")
+
+print("Cool!") if (value < 10) else print("Woah!")
