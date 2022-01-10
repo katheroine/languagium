1889cf2f354df7140ae12c2ac14dd1396d89b434
katheroine <katheroine@gmail.com>
2022-01-10 11:52:07 +0100

    Python concatenation operator substitute [#operators]

---
+++ python/operators/operator_concatenation_substitute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+a = "hello"; b = "world"
+c = a + b
+print(f"{a} + {b} = {c}\n(a: {type(a)}, b: {type(b)}, c: {type(c)})\n")
+
+a = 1; b = 2
+c = a + b
+print(f"{a} + {b} = {c}\n(a: {type(a)}, b: {type(b)}, c: {type(c)})\n")
+
+a = [1, 3, 5]; b = [2, 4]
+c = a + b;
+print(f"{a} + {b} = {c}\n(a: {type(a)}, b: {type(b)}, c: {type(c)})\n")
