0b0af9f9334e54a054b95cb93668679510037d2f
katheroine <katheroine@gmail.com>
2022-01-15 09:57:29 +0100

    Pascal assignment and overwriting value of the array element example modified [#arrays]

--- pascal/arrays/assignment_and_overwriting.pas
+++ pascal/arrays/assignment_and_overwriting.pas
@@ -6,44 +6,26 @@ var
 begin
   writeln('Initialised by zeros, before assignments: some_array = array[0..2] of integer = (0, 0, 0)');
   writeln();
-  write('some_array[0]: ');
-  write(some_array[0]);
-  writeln();
-  write('some_array[1]: ');
-  write(some_array[1]);
-  writeln();
-  write('some_array[2]: ');
-  write(some_array[2]);
-  writeln();
+  writeln('some_array[0]: ', some_array[0]);
+  writeln('some_array[1]: ', some_array[1]);
+  writeln('some_array[2]: ', some_array[2]);
   writeln();
 
   some_array[1] := 6;
 
   writeln('After assignment: some_array[1] := 6');
   writeln();
-  write('some_array[0]: ');
-  write(some_array[0]);
-  writeln();
-  write('some_array[1]: ');
-  write(some_array[1]);
-  writeln();
-  write('some_array[2]: ');
-  write(some_array[2]);
-  writeln();
+  writeln('some_array[0]: ', some_array[0]);
+  writeln('some_array[1]: ', some_array[1]);
+  writeln('some_array[2]: ', some_array[2]);
   writeln();
 
   some_array[1] := 10;
 
   writeln('After assignment: some_array[1] := 10');
   writeln();
-  write('some_array[0]: ');
-  write(some_array[0]);
-  writeln();
-  write('some_array[1]: ');
-  write(some_array[1]);
-  writeln();
-  write('some_array[2]: ');
-  write(some_array[2]);
-  writeln();
+  writeln('some_array[0]: ', some_array[0]);
+  writeln('some_array[1]: ', some_array[1]);
+  writeln('some_array[2]: ', some_array[2]);
   writeln();
 end.
