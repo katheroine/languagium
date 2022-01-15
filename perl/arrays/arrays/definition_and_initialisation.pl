10b7be177ce01865dddcb12f6cd1d070d39a457a
katheroine <katheroine@gmail.com>
2022-01-15 16:41:05 +0100

    Perl arrays definition and initialisation [#arrays]

---
+++ perl/arrays/arrays/definition_and_initialisation.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @array_1 = ();
+
+print("Not initialised, defined as empty by ():\n");
+print(@array_1, "\n\n");
+
+my @array_2 = (2, 4, 6);
+
+print("Initialised, defined as 3-element by ():\n");
+print(@array_2, "\n\n");
