dcacd8b09e027b0d9a7cddf9d1da961e9c9dfa8f
katheroine <katheroine@gmail.com>
2022-01-23 12:00:46 +0100

    Java constants [#types]

---
+++ java/types/Constants.java
@@ -0,0 +1,9 @@
+class Constants {
+  public static void main(String[] args) {
+    final float pi = 3.14f;
+
+    // pi = 3; // error: cannot assign a value to final variable pi
+
+    System.out.print(String.format("pi: %f\n", pi));
+  }
+}
