9da31ebb38b60d881cef109e77b1855edc357625
katheroine <katheroine@gmail.com>
2022-01-24 11:42:56 +0100

    C++ integer literals oct and hex notations example modified [#types]

--- c++/types/integers/integer_literals_oct_and_hex_notations.cpp
+++ c++/types/integers/integer_literals_oct_and_hex_notations.cpp
@@ -4,5 +4,5 @@ int main()
 {
   std::cout << "15: " << 15 << std::endl;
   std::cout << "017: " << 017 << std::endl;
-  std::cout << "0xf / 0Xf / 0xF / 0XF: " << 0xf << " / " << 0Xf << " / " << 0Xf << " / " << 0XF << std::endl << std::endl;
+  std::cout << "0xf / 0Xf / 0xF / 0XF: " << 0xf << " / " << 0Xf << " / " << 0Xf << " / " << 0XF << std::endl;
 }
