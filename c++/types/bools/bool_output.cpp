022433c4c37e905179e57c5fb1b7debb9c13b2ca
katheroine <katheroine@gmail.com>
2022-01-24 10:48:17 +0100

    C++ bool output example modified [#types]

--- c++/types/bools/bool_output.cpp
+++ c++/types/bools/bool_output.cpp
@@ -4,6 +4,11 @@ int main()
 {
   bool a = true, b = false;
 
-  std::cout << "a = " << a << " (" << typeid(a).name() << ")\n";
-  std::cout << "b = " << b << " (" << typeid(b).name() << ")\n";
+  std::cout
+    << "a = " << a << " (" << typeid(a).name() << ")\n"
+    << "b = " << b << " (" << typeid(b).name() << ")\n"
+    << std::boolalpha
+    << "a = " << a << " (" << typeid(a).name() << ")\n"
+    << "b = " << b << " (" << typeid(b).name() << ")\n"
+    << std::noboolalpha;
 }
