dada30ee8fbc0122c5d54341c903922da43a5cf4
katheroine <katheroine@gmail.com>
2022-01-26 16:37:30 +0100

    Java floating point size exampled modified [#types]

--- java/types/floats/FloatingPointSize.java
+++ java/types/floats/FloatingPointSize.java
@@ -1,6 +1,5 @@
 class FloatingPointSize {
   public static void main(String[] args) {
-
     System.out.print(String.format("Float.BYTES: %d\n", Float.BYTES));
     System.out.print(String.format("Double.BYTES: %d\n", Double.BYTES));
   }
