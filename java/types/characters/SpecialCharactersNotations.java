3fd47674d7043c8f41672dd2fcafa9a6976aaa56
katheroine <katheroine@gmail.com>
2022-01-26 15:01:41 +0100

    Java special characters notations [#types]

---
+++ java/types/characters/SpecialCharactersNotations.java
@@ -0,0 +1,11 @@
+class SpecialCharactersNotations {
+  public static void main(String[] args) {
+    System.out.print(String.format("backspace \\b: %c\n", '\b'));
+    System.out.print(String.format("carriage return \\r: \n", '\r'));
+    System.out.print(String.format("new page \\f: \n", '\f'));
+    System.out.print(String.format("new line \\n: \n", '\n'));
+    System.out.print(String.format("horizontal tabulation \\t: \n", '\t'));
+    // System.out.print(String.format("vertical tabulation \\v: \n", '\v'));
+    // System.out.print(String.format("sound signal \\a: \n", '\a'));
+  }
+}
