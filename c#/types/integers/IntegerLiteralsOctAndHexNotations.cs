54e0d07fea19f2b82160e1b49e963c624026af87
katheroine <katheroine@gmail.com>
2022-01-26 13:24:23 +0100

    C# integer literals oct and hex notations example modified [#types]

--- c#/types/integers/IntegerLiteralsOctAndHexNotations.cs
+++ c#/types/integers/IntegerLiteralsOctAndHexNotations.cs
@@ -3,7 +3,7 @@ using System;
 class IntegerLiteralsOctAndHexNotations {
   static void Main(String[] args) {
     System.Console.WriteLine(String.Format("15: {0}", 15));
-    System.Console.WriteLine(String.Format("017: {0}", 017));
+    // System.Console.WriteLine(String.Format("017: {0}", 017)); // doesn't work
     System.Console.WriteLine(String.Format("0xf / 0Xf / 0xF / 0XF: {0} / {1} / {2} / {3}", 0xf, 0Xf, 0xF, 0XF));
   }
 }
