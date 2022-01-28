48c7421a9c012a0057d804d1ae3e9601136e05a7
katheroine <katheroine@gmail.com>
2022-01-28 11:45:41 +0100

    C++ c-string size example modified [#types]

--- c++/types/cstrings/cstring_size.cpp
+++ c++/types/cstrings/cstring_size.cpp
@@ -6,7 +6,7 @@ int main()
   signed char scs[4];
   unsigned char ucs[4];
   wchar_t wcst[4];
-  //char8_t cs8t[4];
+  char8_t cs8t[4];
   char16_t cs16t[4];
   char32_t cs32t[4];
 
@@ -20,8 +20,8 @@ int main()
 	std::cout << "wchar_t wcst[4]; sizeof(wcst): " << sizeof(wcst) << std::endl
     << "sizeof(L\"abc\"): " << sizeof(L"abc") << std::endl << std::endl;
 
-	//std::cout << "char8_t cs8t[4]; sizeof(cs8t): " << sizeof(cs8t) << std::endl
-  //  << "sizeof(u8"abc)" << sizeof(u8"abc") << std::endl << std::endl;
+	std::cout << "char8_t cs8t[4]; sizeof(cs8t): " << sizeof(cs8t) << std::endl
+   << "sizeof(u8\"abc\")" << sizeof(u8"abc") << std::endl << std::endl;
 
 	std::cout << "char16_t cs16t[4]; sizeof(cs16t): " << sizeof(cs16t) << std::endl
     << "sizeof(u\"abc\"): " << sizeof(u"abc") << std::endl << std::endl;
