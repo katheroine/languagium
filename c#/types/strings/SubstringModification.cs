3aeea64c1e2e327ad422f919d81e739c358a5e5e
katheroine <katheroine@gmail.com>
2022-01-29 11:03:18 +0100

    C# modification of the substring [#types]

---
+++ c#/types/strings/SubstringModification.cs
@@ -0,0 +1,10 @@
+class SubstringModification {
+  public static void Main(string[] args) {
+    string cite = "Stat rosa pristina nomine, nomina nuda tenemus.";
+    System.Console.WriteLine(string.Format("cite = {0}\n", cite));
+
+    string modified_cite = cite.Replace("rosa", "odonata");
+
+    System.Console.WriteLine(string.Format("modified_cite = {0}\n", modified_cite));
+  }
+}
