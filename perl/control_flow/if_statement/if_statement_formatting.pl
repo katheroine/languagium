4dac0a9859b0c2cbd31e7f46cedeb7ab5d2e01d5
katheroine <katheroine@gmail.com>
2022-01-30 06:58:22 +0100

    Perl if statement formatting [#control_flow]

---
+++ perl/control_flow/if_statement/if_statement_formatting.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+if (1 > 2) {
+  print("1 > 2\n");
+}
+
+if (1 > 2) { print("1 > 2\n"); }
+
+if (2 > 1) {
+  print("2 > 1\n");
+}
+
+if (2 > 1) { print("2 > 1\n"); }
