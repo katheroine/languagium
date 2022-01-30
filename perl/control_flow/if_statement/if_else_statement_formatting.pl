b9325b025579d6115c754d6067fda013e9b5001a
katheroine <katheroine@gmail.com>
2022-01-30 13:56:25 +0100

    Perl if-else statement formatting [#control_flow]

---
+++ perl/control_flow/if_statement/if_else_statement_formatting.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+if (1 > 2) {
+  print("1 > 2\n");
+} else {
+  print("!(1 > 2)\n");
+}
+
+if (1 > 2) { print("1 > 2\n"); }
+else { print("!(1 > 2)\n"); }
+
+if (2 > 1) {
+  print("2 > 1\n");
+} else {
+  print("!(2 > 1)\n");
+}
+
+if (2 > 1) { print("2 > 1\n"); }
+else { print("!(2 > 1)\n"); }
