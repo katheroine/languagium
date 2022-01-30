768168f4c9a45c5860493735c2043a1c44aa10d6
katheroine <katheroine@gmail.com>
2022-01-30 13:05:47 +0100

    Java if-else statement formatting [#control_flow]

---
+++ java/control_flow/if_statement/IfElseStatementFormatting.java
@@ -0,0 +1,31 @@
+class IfElseStatementFormatting {
+  public static void main(String[] args) {
+    if (1 > 2)
+      System.out.println("1 > 2");
+    else
+      System.out.println("!(1 > 2)");
+
+    if (1 > 2) System.out.println("1 > 2");
+    else System.out.println("!(1 > 2)");
+
+    if (1 > 2) {
+      System.out.println("1 > 2");
+    } else {
+      System.out.println("!(1 > 2)");
+    }
+
+    if (2 > 1)
+      System.out.println("2 > 1");
+    else
+      System.out.println("!(2 > 1)");
+
+    if (2 > 1) System.out.println("2 > 1");
+    else System.out.println("!(2 > 1)");
+
+    if (2 > 1) {
+      System.out.println("2 > 1");
+    } else {
+      System.out.println("!(2 > 1)");
+    }
+  }
+}
