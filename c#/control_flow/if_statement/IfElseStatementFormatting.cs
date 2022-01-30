3dc54fcdc571a1667fd2541647df4d9751e385d3
katheroine <katheroine@gmail.com>
2022-01-30 13:06:34 +0100

    C# if-else statement formatting [#control_flow]

---
+++ c#/control_flow/if_statement/IfElseStatementFormatting.cs
@@ -0,0 +1,37 @@
+class IfElseStatementFormatting {
+  private static void Main(string[] args) {
+    if (1 > 2)
+      System.Console.WriteLine("1 > 2");
+    else
+      System.Console.WriteLine("!(1 > 2)");
+
+    if (1 > 2) System.Console.WriteLine("1 > 2");
+    else System.Console.WriteLine("!(1 > 2)");
+
+    if (1 > 2)
+    {
+      System.Console.WriteLine("1 > 2");
+    }
+    else
+    {
+      System.Console.WriteLine("!(1 > 2)");
+    }
+
+    if (2 > 1)
+      System.Console.WriteLine("2 > 1");
+    else
+      System.Console.WriteLine("!(2 > 1)");
+
+    if (2 > 1) System.Console.WriteLine("2 > 1");
+    else System.Console.WriteLine("!(2 > 1)");
+
+    if (2 > 1)
+    {
+      System.Console.WriteLine("2 > 1");
+    }
+    else
+    {
+      System.Console.WriteLine("!(2 > 1)");
+    }
+  }
+}
