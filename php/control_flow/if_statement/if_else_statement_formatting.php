08026f921c3827f7680cec8ad2f9c0bef5b2c91e
katheroine <katheroine@gmail.com>
2022-01-31 17:12:04 +0100

    PHP if-else statement formatting example modified [#control_flow]

--- php/control_flow/if_statement/if_else_statement_formatting.php
+++ php/control_flow/if_statement/if_else_statement_formatting.php
@@ -2,45 +2,51 @@
 <?php
 
 if (1 > 2)
-  echo "1 > 2\n";
+  print("1 > 2\n");
 else
-  echo "!(1 > 2)\n";
+  print("!(1 > 2)\n");
 
-if (1 > 2) echo "1 > 2\n"; else echo "!(1 > 2)\n";
+if (1 > 2) print("1 > 2\n");
+else print("!(1 > 2)\n");
 
 if (1 > 2) {
-  echo "1 > 2\n";
+  print("1 > 2\n");
 } else {
-  echo "!(1 > 2)\n";
+  print("!(1 > 2)\n");
 }
 
 // Shortened form for HTML templates:
+
 if (1 > 2):
-  echo "1 > 2\n";
+  print("1 > 2\n");
 else:
-  echo "!(1 > 2)\n";
+  print("!(1 > 2)\n");
 endif;
 
-if (1 > 2):  echo "1 > 2\n"; else: echo "!(1 > 2)\n"; endif;
+if (1 > 2):  print("1 > 2\n");
+else: print("!(1 > 2)\n"); endif;
 
 if (2 > 1)
-  echo "2 > 1\n";
+  print("2 > 1\n");
 else
-  echo "!(2 > 1)\n";
+  print("!(2 > 1)\n");
 
-if (2 > 1) echo "2 > 1\n"; else echo "!(2 > 1)\n";
+if (2 > 1) print("2 > 1\n");
+else print("!(2 > 1)\n");
 
 if (2 > 1) {
-  echo "2 > 1\n";
+  print("2 > 1\n");
 } else {
-  echo "!(2 > 1)\n";
+  print("!(2 > 1)\n");
 }
 
 // Shortened form for HTML templates:
+
 if (2 > 1):
-  echo "2 > 1\n";
+  print("2 > 1\n");
 else:
-  echo "!(2 > 1)\n";
+  print("!(2 > 1)\n");
 endif;
 
-if (2 > 1):  echo "2 > 1\n"; else: echo "!(2 > 1)\n"; endif;
+if (2 > 1):  print("2 > 1\n");
+else: print("!(2 > 1)\n"); endif;
