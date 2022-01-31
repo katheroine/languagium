8c09c6b372539fa600c6ed2f22f0141d9ae9d892
katheroine <katheroine@gmail.com>
2022-01-31 13:30:29 +0100

    Java while loop formatting [#control_flow]

---
+++ java/control_flow/while_loop/WhileLoopFormatting.java
@@ -0,0 +1,33 @@
+class WhileLoopFormatting {
+  public static void main(String[] args) {
+    int i = 0;
+
+    while (i < 10) {
+      System.out.println(i + "...");
+      i++;
+    }
+
+    System.out.println();
+
+    i = 0;
+
+    while (i < 10) {
+      System.out.println(i++ + "...");
+    }
+
+    System.out.println();
+
+    i = 0;
+
+    while (i < 10)
+      System.out.println(i++ + "...");
+
+    System.out.println();
+
+    i = 0;
+
+    while (i < 10) System.out.println(i++ + "...");
+
+    System.out.println();
+  }
+}
