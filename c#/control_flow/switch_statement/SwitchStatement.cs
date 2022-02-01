b31a4a7c19faf16162fc75d396c00328d6b80345
katheroine <katheroine@gmail.com>
2022-02-01 19:20:01 +0100

    C# switch statement [#control_flow]

---
+++ c#/control_flow/switch_statement/SwitchStatement.cs
@@ -0,0 +1,65 @@
+class SwitchStatement {
+  enum PartsOfTheDay {
+    morning,
+    afternoon,
+    evening,
+    night,
+    other
+  };
+
+  public static void Main(string[] args) {
+    PartsOfTheDay now;
+
+    now = PartsOfTheDay.afternoon;
+
+    switch (now) {
+      case PartsOfTheDay.morning:
+        System.Console.WriteLine("Good morning!");
+        break;
+      case PartsOfTheDay.afternoon:
+        System.Console.WriteLine("Good afternoon!");
+        break;
+      case PartsOfTheDay.evening:
+        System.Console.WriteLine("Good evening!");
+        break;
+      case PartsOfTheDay.night:
+        System.Console.WriteLine("Good evening!");
+        break;
+    }
+
+    now = PartsOfTheDay.evening;
+
+    switch (now) {
+      case PartsOfTheDay.morning:
+        System.Console.WriteLine("Good morning!");
+        break;
+      case PartsOfTheDay.afternoon:
+        System.Console.WriteLine("Good afternoon!");
+        break;
+      case PartsOfTheDay.evening:
+      case PartsOfTheDay.night:
+        System.Console.WriteLine("Good evening!");
+        break;
+    }
+
+    now = PartsOfTheDay.other;
+
+    switch (now) {
+      case PartsOfTheDay.morning:
+        System.Console.WriteLine("Good morning!");
+        break;
+      case PartsOfTheDay.afternoon:
+        System.Console.WriteLine("Good afternoon!");
+        break;
+      case PartsOfTheDay.evening:
+        System.Console.WriteLine("Good evening!");
+        break;
+      case PartsOfTheDay.night:
+        System.Console.WriteLine("Good evening!");
+        break;
+      default:
+        System.Console.WriteLine("Hello!");
+        break;
+    }
+  }
+}
