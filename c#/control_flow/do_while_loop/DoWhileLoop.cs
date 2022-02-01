fd98f362e38acd0c5ffd932890fc3a582415fc05
katheroine <katheroine@gmail.com>
2022-02-01 10:24:37 +0100

    C# do-while loop [#control_flow]

---
+++ c#/control_flow/do_while_loop/DoWhileLoop.cs
@@ -0,0 +1,11 @@
+class DoWhileLoop {
+  public static void Main(string[] args) {
+    int i = 0;
+
+    do
+    {
+      System.Console.WriteLine(i + "...");
+      i++;
+    } while (i < 10);
+  }
+}
