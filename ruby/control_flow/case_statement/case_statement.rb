f104cedb27a6b1dd0d3e9eb8f610319ea43e15a8
katheroine <katheroine@gmail.com>
2022-02-01 19:46:40 +0100

    Ruby case statement example modified [#control_flow]

--- ruby/control_flow/case_statement/case_statement.rb
+++ ruby/control_flow/case_statement/case_statement.rb
@@ -3,40 +3,38 @@
 now = "afternoon"
 
 case now
-when "morning"
-  puts("Good morning!")
-when "afternoon"
-  puts("Good afternoon!")
-when "evening"
-  puts("Good evening!")
-when "night"
-  puts("Good evening!")
+  when "morning"
+    puts("Good morning!")
+  when "afternoon"
+    puts("Good afternoon!")
+  when "evening"
+    puts("Good evening!")
+  when "night"
+    puts("Good evening!")
 end
 
 now = "evening"
 
 case now
-when "morning"
-  puts("Good morning!")
-when "afternoon"
-  puts("Good afternoon!")
-when "evening" || "night"
-  puts("Good evening!")
+  when "morning"
+    puts("Good morning!")
+  when "afternoon"
+    puts("Good afternoon!")
+  when "evening" || "night"
+    puts("Good evening!")
 end
 
 now = "other"
 
 case now
-when "morning"
-  puts("Good morning!")
-when "afternoon"
-  puts("Good afternoon!")
-when "evening"
-  puts("Good evening!")
-when "night"
-  puts("Good evening!")
-else
-  puts("Hello!")
+  when "morning"
+    puts("Good morning!")
+  when "afternoon"
+    puts("Good afternoon!")
+  when "evening"
+    puts("Good evening!")
+  when "night"
+    puts("Good evening!")
+  else
+    puts("Hello!")
 end
-
-puts()
