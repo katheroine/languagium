1ba4431ebb8b291318117df5b6f2ed3231813e80
katheroine <katheroine@gmail.com>
2022-02-03 15:52:36 +0100

    Java for like foreach loop [#control_flow]

---
+++ java/control_flow/for_loop/ForLikeForeachLoop.java
@@ -0,0 +1,8 @@
+class ForLikeForeachLoop {
+  public static void main(String[] args) {
+    int[] a = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+    for (int i : a)
+      System.out.println(i + "...");
+  }
+}
