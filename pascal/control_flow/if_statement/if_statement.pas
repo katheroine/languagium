12d3001e4f65c86aa0ec68a640ca4fb58d86e0ac
katheroine <katheroine@gmail.com>
2022-02-04 19:25:55 +0100

    Pascal if statement examples modified [#control_flow]

--- pascal/control_flow/if_statement/if_statement.pas
+++ pascal/control_flow/if_statement/if_statement.pas
@@ -1,26 +1,9 @@
 program IfStatement;
 
-var
-  condition: boolean;
-
 begin
   if (1 > 2) then
     writeln('1 > 2');
 
   if (2 > 1) then
     writeln('2 > 1');
-
-  writeln();
-
-  condition := false;
-
-  if (condition) then
-    writeln('false: ok');
-
-  condition := true;
-
-  if (condition) then
-    writeln('true: ok');
-
-  writeln();
 end.
--- pascal/control_flow/if_statement/if_statement_formatting.pas
+++ pascal/control_flow/if_statement/if_statement_formatting.pas
@@ -1,20 +1,33 @@
 program IfStatementFormatting;
 
+var
+  condition: boolean;
+
 begin
+  condition := (1 > 2);
+
+  if (condition) then
+    writeln('1 > 2');
+
+  condition := (2 > 1);
+
+  if (condition) then
+    writeln('2 > 1');
+
   if (1 > 2) then
     writeln('1 > 2');
 
+  if (2 > 1) then
+    writeln('2 > 1');
+
   if (1 > 2) then writeln('1 > 2');
+
+  if (2 > 1) then writeln('2 > 1');
 
   if (1 > 2) then
   begin
     writeln('1 > 2');
   end;
-
-  if (2 > 1) then
-    writeln('2 > 1');
-
-  if (2 > 1) then writeln('2 > 1');
 
   if (2 > 1) then
   begin
