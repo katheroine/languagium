60db5332bf827190009de6e971f987ac02fdda27
katheroine <katheroine@gmail.com>
2022-02-04 20:05:00 +0100

    Pascal if statement formatting example modified [#control_flow]

--- pascal/control_flow/if_statement/if_statement_formatting.pas
+++ pascal/control_flow/if_statement/if_statement_formatting.pas
@@ -14,16 +14,22 @@ begin
   if (condition) then
     writeln('2 > 1');
 
+  writeln();
+
   if (1 > 2) then
     writeln('1 > 2');
 
   if (2 > 1) then
     writeln('2 > 1');
+
+  writeln();
 
   if (1 > 2) then writeln('1 > 2');
 
   if (2 > 1) then writeln('2 > 1');
 
+  writeln();
+
   if (1 > 2) then
   begin
     writeln('1 > 2');
@@ -33,4 +39,6 @@ begin
   begin
     writeln('2 > 1');
   end;
+
+  writeln();
 end.
