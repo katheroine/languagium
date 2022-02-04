a997da1ea66d0ca573ff7f326edee4a34a1ab253
katheroine <katheroine@gmail.com>
2022-02-04 01:49:20 +0100

    Perl foreach loop formatting [#control_flow]

---
+++ perl/control_flow/foreach_loop/foreach_loop_formatting.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @a = (10, 20, 30, 40, 50, 60, 70, 80, 90);
+
+foreach (@a) {
+  print("$_...\n");
+}
+
+print("\n");
+
+foreach (@a) { print("$_...\n"); }
+
+print("\n");
