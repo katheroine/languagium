fcedc25419823847aabe4894c2846d1a5f7f7b03
katheroine <katheroine@gmail.com>
2022-02-05 17:39:04 +0100

    JavaScript if statement examples modified [#control_flow]

--- javascript/control_flow/if_statement/if_statement_formatting.js
+++ javascript/control_flow/if_statement/if_statement_formatting.js
@@ -1,19 +1,37 @@
 #!/usr/bin/env node
 
+let condition = (1 > 2);
+
+if (condition)
+  console.log('1 > 2');
+
+condition = (2 > 1);
+
+if (condition)
+  console.log('2 > 1');
+
+console.log();
+
 if (1 > 2)
   console.log('1 > 2');
 
+if (2 > 1)
+  console.log('2 > 1');
+
+console.log();
+
 if (1 > 2) console.log('1 > 2');
+
+if (2 > 1) console.log('2 > 1');
+
+console.log();
 
 if (1 > 2) {
   console.log('1 > 2');
 }
 
-if (2 > 1)
-  console.log('2 > 1');
-
-if (2 > 1) console.log('2 > 1');
-
 if (2 > 1) {
   console.log('2 > 1');
 }
+
+console.log();
