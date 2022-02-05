40e2a327c5e56c7e8e754c0b3ce1d8add2f56f1b
katheroine <katheroine@gmail.com>
2022-02-05 23:49:39 +0100

    Ruby if-else statement examples modified [#control_flow]

--- ruby/control_flow/if_statement/if_else_statement.rb
+++ ruby/control_flow/if_statement/if_else_statement.rb
@@ -1,33 +1,13 @@
 #!/usr/bin/ruby
 
 if 1 > 2
-    puts '1 > 2'
+  puts '1 > 2'
 else
-    puts '!(1 > 2)'
+  puts '!(1 > 2)'
 end
 
 if 2 > 1
-    puts '2 > 1'
+  puts '2 > 1'
 else
-    puts '!(2 > 1)'
+  puts '!(2 > 1)'
 end
-
-puts
-
-condition = false
-
-if condition
-  puts 'false: ok'
-else
-  puts 'false: nope'
-end
-
-condition = true
-
-if condition
-  puts 'true: ok'
-else
-  puts 'true: nope'
-end
-
-puts
--- ruby/control_flow/if_statement/if_else_statement_formatting.rb
+++ ruby/control_flow/if_statement/if_else_statement_formatting.rb
@@ -1,14 +1,28 @@
 #!/usr/bin/ruby
 
+condition = 1 > 2
+
+if condition
+  puts '1 > 2'
+else
+  puts '!(1 > 2)'
+end
+
+condition = 2 > 1
+
+if condition
+  puts '2 > 1'
+else
+  puts '!(2 > 1)'
+end
+
+puts
+
 if 1 > 2
     puts '1 > 2'
 else
     puts '!(1 > 2)'
 end
-
-if 1 > 2 then puts '1 > 2' else puts '!(1 > 2)' end
-
-if 1 > 2; puts '1 > 2' else puts '!(1 > 2)' end
 
 if 2 > 1
     puts '2 > 1'
@@ -16,6 +30,16 @@ else
     puts '!(2 > 1)'
 end
 
+puts
+
+if 1 > 2 then puts '1 > 2' else puts '!(1 > 2)' end
+
 if 2 > 1 then puts '2 > 1' else puts '!(2 > 1)' end
 
+puts
+
+if 1 > 2; puts '1 > 2' else puts '!(1 > 2)' end
+
 if 2 > 1; puts '2 > 1' else puts '!(2 > 1)' end
+
+puts
