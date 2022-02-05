5bfe8c3172b3a99553ad3dbb80e71251d04dedca
katheroine <katheroine@gmail.com>
2022-02-05 23:56:47 +0100

    Python if statement examples modified [#control_flow]

--- python/control_flow/if_statement/if_statement.py
+++ python/control_flow/if_statement/if_statement.py
@@ -5,17 +5,3 @@ if 1 > 2:
 
 if 2 > 1:
     print('2 > 1')
-
-print()
-
-condition = False
-
-if condition:
-    print('False: ok')
-
-condition = True
-
-if condition:
-    print('True: ok')
-
-print()
--- python/control_flow/if_statement/if_statement_formatting.py
+++ python/control_flow/if_statement/if_statement_formatting.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
-if 1 > 2:
+condition = 1 > 2
+
+if condition:
     print('1 > 2')
 
-if 2 < 1: print('2 > 1')
+condition = 2 > 1
+
+if condition:
+    print('2 > 1')
+
+print()
+
+if 1 > 2:
+    print('1 > 2')
 
 if 2 > 1:
     print('2 > 1')
 
+print()
+
+if 2 < 1: print('2 > 1')
+
 if 2 > 1: print('2 > 1')
+
+print()
