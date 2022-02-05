28d376c510efced14c0c8fda8ec5502189ef89cf
katheroine <katheroine@gmail.com>
2022-02-05 15:05:53 +0100

    Java if statement examples modified [#control_flow]

--- java/control_flow/if_statement/IfStatement.java
+++ java/control_flow/if_statement/IfStatement.java
@@ -1,25 +1,9 @@
 class IfStatement {
   public static void main(String args[]) {
     if (1 > 2)
-      System.out.print("1 > 2\n");
+      System.out.println("1 > 2");
 
     if (2 > 1)
-      System.out.print("2 > 1\n");
-
-    System.out.print("\n");
-
-    boolean condition;
-
-    condition = false;
-
-    if (condition)
-      System.out.print("false: ok\n");
-
-    condition = true;
-
-    if (condition)
-      System.out.print("true: ok\n");
-
-    System.out.print("\n");
+      System.out.println("2 > 1");
   }
 }
--- java/control_flow/if_statement/IfStatementFormatting.java
+++ java/control_flow/if_statement/IfStatementFormatting.java
@@ -1,21 +1,41 @@
 class IfStatementFormatting {
   public static void main(String[] args) {
+    boolean condition;
+
+    condition = (1 > 2);
+
+    if (condition)
+      System.out.println("1 > 2");
+
+    condition = (2 > 1);
+
+    if (condition)
+      System.out.println("2 > 1");
+
+    System.out.println();
+
     if (1 > 2)
-      System.out.print("1 > 2\n");
-
-    if (1 > 2) System.out.print("1 > 2\n");
-
-    if (1 > 2) {
-      System.out.print("1 > 2\n");
-    }
+      System.out.println("1 > 2");
 
     if (2 > 1)
-      System.out.print("2 > 1\n");
+      System.out.println("2 > 1");
 
-    if (2 > 1) System.out.print("2 > 1\n");
+    System.out.println();
+
+    if (1 > 2) System.out.println("1 > 2");
+
+    if (2 > 1) System.out.println("2 > 1");
+
+    System.out.println();
+
+    if (1 > 2) {
+      System.out.println("1 > 2");
+    }
 
     if (2 > 1) {
-      System.out.print("2 > 1\n");
+      System.out.println("2 > 1");
     }
+
+    System.out.println();
   }
 }
