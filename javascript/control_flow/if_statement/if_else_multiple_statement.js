787f1fe88a5ac7686c0028fed3b40a8aa68731aa
katheroine <katheroine@gmail.com>
2022-02-05 17:59:34 +0100

    JavaScript if-else multiple statement example modified [#control_flow]

--- javascript/control_flow/if_statement/if_else_multiple_statement.js
+++ javascript/control_flow/if_statement/if_else_multiple_statement.js
@@ -1,25 +1,22 @@
 #!/usr/bin/env node
 
-if (1 > 2) {
+if (1 > 2)
   console.log('1 > 2');
-} else if (2 > 3) {
+else if (2 > 3)
   console.log('2 > 3');
-} else {
+else
   console.log('!(1 > 2) && !(2 > 3)');
-}
 
-if (1 > 2) {
+if (1 > 2)
   console.log('1 > 2');
-} else if (1 > 0) {
+else if (1 > 0)
   console.log('1 > 0');
-} else {
+else
   console.log('!(1 > 2) && !(1 > 0)');
-}
 
-if (2 > 1) {
+if (2 > 1)
   console.log('2 > 1');
-} else if (1 > 0) {
+else if (1 > 0)
   console.log('1 > 0');
-} else {
+else
   console.log('!(2 > 1) && !(1 > 0)');
-}
