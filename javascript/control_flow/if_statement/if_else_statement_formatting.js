68aae174bd975b7bdb4ed58310f9cee468d11794
katheroine <katheroine@gmail.com>
2022-02-05 17:51:21 +0100

    JavaScript if-else statement examples modified [#control_flow]

--- javascript/control_flow/if_statement/if_else_statement_formatting.js
+++ javascript/control_flow/if_statement/if_else_statement_formatting.js
@@ -1,11 +1,38 @@
 #!/usr/bin/env node
 
+let condition = (1 > 2);
+
+if (condition)
+  console.log('1 > 2');
+else
+  console.log('!(1 > 2)');
+
+condition = (2 > 1);
+
+if (condition)
+  console.log('2 > 1');
+else
+  console.log('!(2 > 1)');
+
+console.log();
+
 if (1 > 2)
   console.log('1 > 2');
 else
   console.log('!(1 > 2)');
 
+if (2 > 1)
+  console.log('2 > 1');
+else
+  console.log('!(2 > 1)');
+
+console.log();
+
 if (1 > 2) console.log('1 > 2'); else console.log('!(1 > 2)');
+
+if (2 > 1) console.log('2 > 1'); else console.log('!(2 > 1)');
+
+console.log();
 
 if (1 > 2) {
   console.log('1 > 2');
@@ -13,15 +40,10 @@ if (1 > 2) {
   console.log('!(1 > 2)');
 }
 
-if (2 > 1)
-  console.log('2 > 1');
-else
-  console.log('!(2 > 1)');
-
-if (2 > 1) console.log('2 > 1'); else console.log('!(2 > 1)');
-
 if (2 > 1) {
   console.log('2 > 1');
 } else {
   console.log('!(2 > 1)');
 }
+
+console.log();
