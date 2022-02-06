8d9a31ba1cb61241c10781f662e5831affa3886e
katheroine <katheroine@gmail.com>
2022-02-06 11:37:20 +0100

    PHP if-else multiple statement example modified [#control_flow]

--- php/control_flow/if_statement/if_else_multiple_statement.php
+++ php/control_flow/if_statement/if_else_multiple_statement.php
@@ -3,21 +3,21 @@
 
 if (1 > 2)
   print("1 > 2\n");
-else if (2 > 3)
+elseif (2 > 3)
   print("2 > 3\n");
 else
   print("!(1 > 2) && !(2 > 3)\n");
 
 if (1 > 2)
   print("1 > 2\n");
-else if (1 > 0)
+elseif (1 > 0)
   print("1 > 0\n");
 else
   print("!(1 > 2) && !(1 > 0)\n");
 
 if (2 > 1)
   print("2 > 1\n");
-else if (1 > 0)
+elseif (1 > 0)
   print("1 > 0\n");
 else
   print("!(2 > 1) && !(1 > 0)\n");
