f2adcca4a9708668586afa268032c0d139976ba5
katheroine <katheroine@gmail.com>
2022-02-06 17:22:49 +0100

    C# continue instruction [#control_flow]

---
+++ c#/control_flow/continue_instruction/ContinueInstruction.cs
@@ -0,0 +1,16 @@
+class ContinueInstruction {
+  public static void Main(string[] args) {
+    int i = 0;
+
+    while (i < 10)
+    {
+      System.Console.WriteLine(i + "...");
+      i++;
+
+      if (i > 5)
+        continue;
+
+      System.Console.WriteLine("*********************");
+    }
+  }
+}
