19ec69ad20a11690579e16cb2299aa778cde7829
katheroine <katheroine@gmail.com>
2022-02-07 14:34:56 +0100

    Perl unless-else statement formatting [#control_flow]

---
+++ perl/control_flow/unless_statement/unless_else_statement_formtting.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $condition = (1 > 2);
+
+unless ($condition) {
+  print("!(1 > 2)\n");
+} else {
+  print("1 > 2\n");
+}
+
+$condition = (2 > 1);
+
+unless ($condition) {
+  print("!(2 > 1)\n");
+} else {
+  print("2 > 1\n");
+}
+
+print("\n");
+
+unless (1 > 2) {
+  print("!(1 > 2)\n");
+} else {
+  print("1 > 2\n");
+}
+
+unless (2 > 1) {
+  print("!(2 > 1)\n");
+} else {
+  print("2 > 1\n");
+}
+
+print("\n");
+
+unless (1 > 2) { print("!(1 > 2)\n"); }
+else { print("1 > 2\n"); }
+
+unless (2 > 1) { print("!(2 > 1)\n"); }
+else { print("2 > 1\n"); }
+
+print("\n");
