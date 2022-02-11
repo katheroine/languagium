54f7b87ad46922533c39d917dbe37aa0a784bc78
katheroine <katheroine@gmail.com>
2022-02-11 17:13:30 +0100

    Perl foreach loop iterator value restoring [#control_flow]

---
+++ perl/control_flow/foreach_loop/foreach_loop_iterator_value_restoring.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @a = (10, 20, 30, 40, 50, 60, 70, 80, 90);
+my $e = 15;
+
+print("$e\n");
+
+foreach my $e (@a) {
+  print("$e...\n");
+}
+
+print("$e\n");
