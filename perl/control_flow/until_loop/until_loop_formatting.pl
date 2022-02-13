b76101e79bae4c43d1a99349009a03fab3e2f546
katheroine <katheroine@gmail.com>
2022-02-13 12:06:01 +0100

    Perl until loop formatting [#control_flow]

---
+++ perl/control_flow/until_loop/until_loop_formatting.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $i = 0;
+
+until ($i > 9) {
+  print("$i...\n");
+  $i++;
+}
+
+print("\n");
+
+$i = 0;
+
+until ($i > 9) {
+  print($i++ . "...\n");
+}
+
+print("\n");
+
+$i = 0;
+
+until ($i > 9) { print($i++ . "...\n"); }
+
+print("\n");
