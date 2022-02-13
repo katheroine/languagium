fc2558b635d6fab1983a3277c02817480fb98e66
katheroine <katheroine@gmail.com>
2022-02-13 12:28:15 +0100

    Perl until loop statement modifier [#control_flow]

--- perl/control_flow/until_loop/until_loop_formatting.pl
+++ perl/control_flow/until_loop/until_loop_formatting.pl
@@ -25,3 +25,9 @@ $i = 0;
 until ($i > 9) { print($i++ . "...\n"); }
 
 print("\n");
+
+$i = 0;
+
+print($i++ . "...\n") until ($i > 9);
+
+print("\n");
---
+++ perl/control_flow/until_loop/until_loop_statement_modifier.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $i = 0;
+
+print($i++ . "...\n") until ($i > 9);
