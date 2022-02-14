d2f19eadc1f260b79f08121b696c69f213cb537d
katheroine <katheroine@gmail.com>
2022-02-14 17:33:57 +0100

    Ruby while loop statement modifier [#control_flow]

--- ruby/control_flow/while_loop/while_loop_formatting.rb
+++ ruby/control_flow/while_loop/while_loop_formatting.rb
@@ -31,3 +31,9 @@ i = 0
 while i < 10; puts("#{i}..."); i += 1; end
 
 puts
+
+i = -1
+
+puts("#{i += 1}...") while i < 9
+
+puts
---
+++ ruby/control_flow/while_loop/while_loop_statement_modifier.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+i = -1
+
+puts("#{i += 1}...") while i < 9
