5e9531c0047176b5eea30e9421e19c68b10d62cf
katheroine <katheroine@gmail.com>
2022-02-23 13:57:32 +0100

    C++ arrays definition and initialisation example modified [#arrays]

--- c++/arrays/definition_and_initialisation.cpp
+++ c++/arrays/definition_and_initialisation.cpp
@@ -10,6 +10,7 @@ int main()
   std::cout << "[2]: " << array_1[2] << std::endl << std::endl;
 
   int array_2[] = {2, 4, 6};
+  
   std::cout << "Initialised, defined as 3-element:" << std::endl << std::endl;
   std::cout << "[0]: " << array_2[0] << std::endl;
   std::cout << "[1]: " << array_2[1] << std::endl;
