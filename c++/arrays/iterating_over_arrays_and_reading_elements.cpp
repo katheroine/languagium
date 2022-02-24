c449208009d5361dc9fbc2d504a0867687dd1488
katheroine <katheroine@gmail.com>
2022-02-24 11:01:39 +0100

    C++ iterating over array and reading elements example completed [#arrays]

--- c++/arrays/iterating_over_arrays_and_reading_elements.cpp
+++ c++/arrays/iterating_over_arrays_and_reading_elements.cpp
@@ -4,22 +4,27 @@ int main()
 {
   const char *array[5] = {"apple", "orange", "banana", "pear", "peach"};
 
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 5; i++)
     std::cout << "array[" << i << "]: " << array[i] << std::endl;
 
   std::cout << std::endl;
 
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 5; i++)
     std::cout << "*(array + " << i << "): " << *(array + i) << std::endl;
 
   std::cout << std::endl;
 
   const char **element_pointer = array;
 
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < 5; i++)
     std::cout << "*(element_pointer++): " << *(element_pointer++) << std::endl;
 
   element_pointer = 0;
+
+  std::cout << std::endl;
+
+  for (const char *element : array)
+    std::cout << "element: " << element << std::endl;
 
   std::cout << std::endl;
 }
