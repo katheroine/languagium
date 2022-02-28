fc4a67d3df692a34df0dc9a9de89bbdee41aebb2
katheroine <katheroine@gmail.com>
2022-02-28 18:49:38 +0100

    Ruby function calling function example modified [#functions]

--- ruby/functions/function_calling_function.rb
+++ ruby/functions/function_calling_function.rb
@@ -1,16 +1,18 @@
 #!/usr/bin/ruby2.7
 
 def inside()
-  puts("Inside.")
+  puts("* Inside.")
   return "IN"
 end
 
 def outside()
-  puts("Outside:\n",
-    "Calling function from function...")
-  inside()
+  puts("# Outside:\n",
+    "# Calling function from function...")
+  result = inside()
+  puts("# result: #{result}\n")
   return "OUT"
 end
 
 puts("Calling function...")
-outside()
+result = outside()
+puts("result: #{result}\n")
