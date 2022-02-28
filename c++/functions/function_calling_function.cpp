eea21dae49a1b2327056b90f637b8db3bed6ce22
katheroine <katheroine@gmail.com>
2022-02-28 17:14:19 +0100

    C++ function calling function example modified [#functions]

--- c++/functions/function_calling_function.cpp
+++ c++/functions/function_calling_function.cpp
@@ -2,20 +2,22 @@
 
 const char* inside()
 {
-  std::cout << "Inside.\n";
+  std::cout << "* Inside.\n";
   return "IN";
 }
 
 const char* outside()
 {
-  std::cout << "Outside:\n"
-    "Calling function from function...\n";
-  inside();
+  std::cout << "# Outside:\n"
+    "# Calling function from function...\n";
+  const char* result = inside();
+  std::cout << "# result: " << result << std::endl;
   return "OUT";
 }
 
 int main()
 {
   std::cout << "Calling function...\n";
-  outside();
+  const char* result = outside();
+  std::cout << "result: " << result << std::endl;
 }
