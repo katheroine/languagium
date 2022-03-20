3d5c0f3a9b5eb252b15fc9b2358c42cda8f80bd7
katheroine <katheroine@gmail.com>
2022-03-20 15:02:11 +0100

    C++ classes [#classes]

---
+++ c++/classes/classes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class MoneyBox {
+public:
+  std::string name = "";
+  double saved_amount = 0;
+
+  void save(double amount) {
+    saved_amount += amount;
+  }
+};
+
+int main() {
+  MoneyBox piggy;
+  piggy.name = "Miss Piggy";
+
+  std::cout << "Money box name: " << piggy.name << std::endl;
+  std::cout << "Money box saved amount: " << piggy.saved_amount << std::endl;
+
+  piggy.save(20);
+
+  std::cout << "Money box saved amount: " << piggy.saved_amount << std::endl;
+
+  piggy.save(100);
+
+  std::cout << "Money box saved amount: " << piggy.saved_amount << std::endl;
+}
