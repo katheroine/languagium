3e783d72d88420345d3c61a51d3843a970cafb4a
katheroine <katheroine@gmail.com>
2022-03-21 15:46:27 +0100

    JavaScript classes [#classes]

---
+++ javascript/classes/classes.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+class MoneyBox {
+  constructor() {
+    this.name = "";
+    this.saved_amount = 0;
+    this.save = function(amount) {
+    this.saved_amount += amount;
+  }
+  }
+}
+
+let piggy = new MoneyBox();
+piggy.name = "Miss Piggy";
+
+console.log("Money box name: " + piggy.name);
+console.log("Money box saved amount: " + piggy.saved_amount);
+
+piggy.save(20);
+
+console.log("Money box saved amount: " + piggy.saved_amount);
+
+piggy.save(100);
+
+console.log("Money box saved amount: " + piggy.saved_amount);
