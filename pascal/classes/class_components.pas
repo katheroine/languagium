0b4e9417b4769c0273165d7dc1dbac26935ac71f
katheroine <katheroine@gmail.com>
2022-03-23 16:58:20 +0100

    Pascal class components [#classes]

---
+++ pascal/classes/class_components.pas
@@ -0,0 +1,55 @@
+{$MODE OBJFPC}
+
+program ClassComponents;
+
+type Account = class
+  public
+    login: string;
+    email: string;
+    is_active: boolean;
+    id: integer;
+
+    connections: array[0..9] of integer;
+    connections_number: integer;
+
+    constructor create();
+
+    procedure display();
+    function hasConnections(): boolean;
+end;
+
+constructor Account.create();
+begin
+  login := '';
+  email := '';
+  is_active := false;
+  id := 0;
+  connections_number := 0;
+end;
+
+procedure Account.display();
+begin
+  writeln('ID: ', id);
+  writeln('Login: ', login);
+  writeln('E-mail: ', email);
+  writeln('Is active: ', is_active);
+  writeln('Has connections: ', hasConnections());
+end;
+
+function Account.hasConnections(): boolean;
+begin
+  hasConnections := (connections_number > 0);
+end;
+
+var
+  timothy: Account;
+
+begin
+  timothy := Account.create();
+
+  timothy.login := 'tim';
+  timothy.email := 'timothy.muppetone@gmail.com';
+  timothy.is_active := true;
+
+  timothy.display();
+end.
