ec5ea14167a0d9d0bd7225eb9abf116e2670dfde
katheroine <katheroine@gmail.com>
2022-04-04 13:06:41 +0200

    Python class components example modified [#classes]

--- python/classes/class_components.py
+++ python/classes/class_components.py
@@ -6,15 +6,15 @@ class Account:
     is_active = False
     id = 0
 
-    connsections = []
+    connections = []
     connections_number = 0
 
     def display(self):
-        print("ID: ", self.id,
-          "\nLogin: ", self.login,
-          "\nE-mail: ", self.email,
-          "\nIs active: ", self.is_active,
-          "\nHas connections: ", self.has_connections()
+        print("ID:", self.id,
+          "\nLogin:", self.login,
+          "\nE-mail:", self.email,
+          "\nIs active:", self.is_active,
+          "\nHas connections:", self.has_connections()
         );
 
     def has_connections(self):
