6f4cc12bb08abe5a354169af270d42d7fbb45b9c
katheroine <katheroine@gmail.com>
2022-08-01 13:50:44 +0200

    C++ very simple constructor & destructor [#classes]

---
+++ c++/classes/constructor_and_destructor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Item {
+public:
+  Item() {
+    std::cout << "Constructor is running..." << std::endl;
+  }
+
+  ~Item() {
+    std::cout << "Destructor is running..." << std::endl;
+  }
+
+  void action() {
+    std::cout << "Some action is performing..." << std::endl;
+  }
+};
+
+int main() {
+  std::cout << "The object will be created now."<< std::endl;
+
+  Item thing;
+  thing.action();
+
+  std::cout << "The program will end now." << std::endl;
+}
