fcf688e2579188555efa0f8232bacc7ffcc09376
katheroine <katheroine@gmail.com>
2022-08-03 12:46:00 +0200

    Java very simple constructor [#classes]

---
+++ java/classes/ConstructorAndDestructor.java
@@ -0,0 +1,22 @@
+class Item {
+  public Item() {
+    System.out.println("Constructor is running...");
+  }
+
+  // There is no destructor in Java.
+
+  public void action() {
+    System.out.println("Some action is performing...");
+  }
+}
+
+class ConstructorAndDestructor {
+  public static void main(String args[]) {
+    System.out.println("The object will be created now.");
+
+    Item thing = new Item();
+    thing.action();
+
+    System.out.println("The program will end now.");
+  }
+}
