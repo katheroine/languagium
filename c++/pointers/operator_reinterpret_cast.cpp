0dd3c55c14a14f2e83a4b937383b1b3bb4549d8a
katheroine <katheroine@gmail.com>
2022-09-12 11:36:08 +0200

    C++ operator reinterpret_cast example completed [#pointers]

--- c++/pointers/operator_reinterpret_cast.cpp
+++ c++/pointers/operator_reinterpret_cast.cpp
@@ -4,21 +4,30 @@ int main() {
   int i = 35;
   double d = 47.65;
   int *pi;
-  double *di;
+  double *pd;
+  long int addr = 0;
 
   pi = &i;
-  di = &d;
+  pd = &d;
 
   std::cout << "i: " << i << std::endl
     << "d: " << d << std::endl
     << "*pi: " << *pi << std::endl
-    << "*di: " << *di << std::endl << std::endl;
+    << "*pd: " << *pd << std::endl << std::endl;
 
-  pi = reinterpret_cast<int*>(di);
+  addr = reinterpret_cast<long int>(pi);
+
+  pi = reinterpret_cast<int*>(pd);
 
   std::cout << "*pi: " << *pi << std::endl;
 
   pi = reinterpret_cast<int*>(&d);
+
+  std::cout << "*pi: " << *pi << std::endl;
+
+  std::cout << std::endl;
+
+  pi = reinterpret_cast<int*>(addr);
 
   std::cout << "*pi: " << *pi << std::endl;
 }
