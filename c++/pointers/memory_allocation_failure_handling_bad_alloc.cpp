52dae1ea010d117498a1563d0197caf174167995
katheroine <katheroine@gmail.com>
2022-09-22 18:58:29 +0200

    C++ Memory allocation failure handling: std::bad_alloc exception example completed [#pointers]

--- c++/pointers/memory_allocation_failure_handling_bad_alloc.cpp
+++ c++/pointers/memory_allocation_failure_handling_bad_alloc.cpp
@@ -4,6 +4,7 @@ int main() {
   try {
     double *ptr;
     ptr = new double[10000000000];
+    delete ptr;
   } catch (std::bad_alloc exception) {
       std::cout << "Memory allocation failure has occured!" << std::endl;
   }
