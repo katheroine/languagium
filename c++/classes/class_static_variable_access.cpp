22ba40648160ccbb53ed36168ad3ee9bcc20dad5
katheroine <katheroine@gmail.com>
2022-12-07 14:08:10 +0100

    C++ ways of access to the class static variable [#classes]

---
+++ c++/classes/class_static_variable_access.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+class Token {
+public:
+  static int number;
+  std::string color;
+};
+
+int Token::number;
+
+int main() {
+  Token::number = 4;
+
+  std::cout << "Token::number: " << Token::number << std::endl;
+
+  Token some_token;
+
+  some_token.number = 5;
+  some_token.color = "magenta";
+
+  std::cout << "some_token.number: " << some_token.number << ", some_token.color: " << some_token.color << std::endl;
+
+  Token *pointer_to_token = &some_token;
+
+  (*pointer_to_token).number = 6;
+  (*pointer_to_token).color = "amaranthine";
+
+  std::cout << "(*pointer_to_token).number: " << (*pointer_to_token).number << ", (*pointer_to_token).color: " << (*pointer_to_token).color << std::endl;
+
+  pointer_to_token->number = 7;
+  pointer_to_token->color = "indigo";
+
+  std::cout << "pointer_to_token->number: " << pointer_to_token->number << ", pointer_to_token->color: " << pointer_to_token->color << std::endl;
+}
