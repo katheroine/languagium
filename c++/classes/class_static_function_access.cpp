ad257a7ae5aa2b4f32f22338f41dd69c17e8ce0b
katheroine <katheroine@gmail.com>
2023-01-05 15:25:14 +0100

    C++ ways of access to the class static function example modified [#classes]

--- c++/classes/class_static_function_access.cpp
+++ c++/classes/class_static_function_access.cpp
@@ -18,45 +18,45 @@ int Token::number;
 
 int main() {
   Token::reset();
-
   std::cout << "Token::number: " << Token::number << std::endl;
+
+  std::cout << std::endl;
 
   Token some_token;
 
   some_token.update(5, "magenta");
-
   std::cout << "some_token.number: " << some_token.number << ", some_token.color: " << some_token.color << std::endl;
 
   some_token.reset();
-
   std::cout << "some_token.number: " << some_token.number << ", some_token.color: " << some_token.color << std::endl;
+
+  std::cout << std::endl;
 
   Token *pointer_to_token = &some_token;
 
   (*pointer_to_token).update(6, "amaranthine");
-
   std::cout << "(*pointer_to_token).number: " << (*pointer_to_token).number << ", (*pointer_to_token).color: " << (*pointer_to_token).color << std::endl;
 
   (*pointer_to_token).reset();
-
   std::cout << "(*pointer_to_token).number: " << (*pointer_to_token).number << ", (*pointer_to_token).color: " << (*pointer_to_token).color << std::endl;
 
-  pointer_to_token->update(7, "indigo");
+  std::cout << std::endl;
 
+  pointer_to_token->update(7, "indigo");
   std::cout << "pointer_to_token->number: " << pointer_to_token->number << ", pointer_to_token->color: " << pointer_to_token->color << std::endl;
 
   pointer_to_token->reset();
-
   std::cout << "pointer_to_token->number: " << pointer_to_token->number << ", pointer_to_token->color: " << pointer_to_token->color << std::endl;
+
+  std::cout << std::endl;
 
   Token &reference_to_token = some_token;
 
   reference_to_token.update(8, "navy");
-
   std::cout << "reference_to_token.number: " << reference_to_token.number << ", reference_to_token.color: " << reference_to_token.color << std::endl;
 
   reference_to_token.reset();
-
   std::cout << "reference_to_token.number: " << reference_to_token.number << ", reference_to_token.color: " << reference_to_token.color << std::endl;
 
+  std::cout << std::endl;
 }
