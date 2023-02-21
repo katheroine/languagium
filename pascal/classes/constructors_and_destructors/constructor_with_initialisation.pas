35725d9ee91f623f394f73728273b735ff957106
katheroine <katheroine@gmail.com>
2023-02-21 18:24:04 +0100

    Pascal constructor with initialisation [#classes]

---
+++ pascal/classes/constructors_and_destructors/constructor_with_initialisation.pas
@@ -0,0 +1,70 @@
+{$MODE OBJFPC}
+
+program ConstructorWithInitialisation;
+
+type Basket = class
+  public
+    lbl: string;
+    capacity: integer;
+    items: array[0..2] of string;
+    load_counter: integer;
+
+    constructor create();
+    destructor delete();
+
+    procedure put(item: string);
+    function pull(): string;
+    procedure show();
+end;
+
+constructor Basket.create();
+begin
+  capacity := 3;
+  load_counter := 0;
+end;
+
+destructor Basket.delete();
+begin
+end;
+
+procedure Basket.put(item: string);
+begin
+  if (load_counter < capacity) then
+    items[load_counter] := item;
+
+  inc(load_counter);
+end;
+
+function Basket.pull(): string;
+var
+  item: string;
+begin
+  item := items[load_counter];
+  items[load_counter] := '';
+  dec(load_counter);
+
+  pull := item;
+end;
+
+procedure Basket.show();
+var
+  i: integer;
+begin
+  for i := 0 to high(items) do
+    write(items[i], ' ');
+
+  writeln();
+end;
+
+var
+  handbasket: Basket;
+
+begin
+  handbasket := Basket.create();
+
+  handbasket.put('apple');
+  handbasket.put('pear');
+  handbasket.put('plum');
+
+  handbasket.show();
+end.
