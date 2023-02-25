02446cc54fddb2483a7c730c379ce0220e5b5d83
katheroine <katheroine@gmail.com>
2023-02-25 11:20:13 +0100

    PHP constructor with initialisation example fixed [#classes]

--- php/classes/constructors_and_destructors/constructor_with_initialisation.php
+++ php/classes/constructors_and_destructors/constructor_with_initialisation.php
@@ -2,36 +2,40 @@
 <?php
 
 class Basket {
-  public function __construct() {
-    $this->capacity = 3;
-    $this->load_counter = 0;
-  }
-
-  public function put(string $item): void {
-    if ($this->load_counter < $this->capacity) {
-      $this->items[$this->load_counter] = $item;
+    public function __construct() {
+        $this->capacity = 3;
+        $this->load_counter = 0;
     }
-    $this->load_counter++;
-  }
 
-  public function pull(): string {
-    $this->item = $this->items[$this->load_counter];
-    $this->items[$this->load_counter] = "";
-    $this->load_counter--;
-
-    return $this->item;
-  }
-
-  public function show(): void {
-    foreach ($this->items as $item) {
-      print($item . " ");
+    public function put(string $item): void {
+        if ($this->load_counter < $this->capacity) {
+            $this->items[$this->load_counter] = $item;
+            $this->load_counter++;
+        }
     }
-    print("\n");
-  }
 
-  private int $capacity;
-  private array $items = [];
-  private int $load_counter;
+    public function pull(): string {
+        if ($this->load_counter > 0) {
+            $item = $this->items[$this->load_counter - 1];
+            $this->items[$this->load_counter - 1] = "";
+            $this->load_counter--;
+        } else {
+            $item = "";
+        }
+
+        return $item;
+    }
+
+    public function show(): void {
+        foreach ($this->items as $item) {
+            print($item . " ");
+        }
+        print("\n");
+    }
+
+    private int $capacity;
+    private array $items = [];
+    private int $load_counter;
 }
 
 $handbasket = new Basket();
@@ -39,5 +43,26 @@ $handbasket = new Basket();
 $handbasket->put("apple");
 $handbasket->put("pear");
 $handbasket->put("plum");
+$handbasket->put("peach");
+
+print("BASKET:\n");
+
+$handbasket->show();
+
+print("REMOVED ITEMS:\n");
+
+print($handbasket->pull() . "\n");
+print($handbasket->pull() . "\n");
+
+print("BASKET:" . "\n");
+
+$handbasket->show();
+
+print("REMOVED ITEMS:\n");
+
+print($handbasket->pull() . "\n");
+print($handbasket->pull() . "\n");
+
+print("BASKET:\n");
 
 $handbasket->show();
