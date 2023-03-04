4df7e1259f45da0cf3f3577b5390883995e980c7
katheroine <katheroine@gmail.com>
2023-03-04 16:31:45 +0100

    C++ member single-argument minus operator example completed [#operator_overloading]

--- c++/classes/operator_overloading/member_single_argument_minus.cpp
+++ c++/classes/operator_overloading/member_single_argument_minus.cpp
@@ -6,7 +6,7 @@ public:
     files = new std::string[capacity];
   }
 
-  Folder(Folder &folder_pattern): capacity(folder_pattern.capacity) {
+  Folder(const Folder &folder_pattern): capacity(folder_pattern.capacity) {
     files = new std::string[folder_pattern.capacity];
 
     for (int i = 0; i < folder_pattern.capacity; i++) {
@@ -18,11 +18,11 @@ public:
     delete [] files;
   }
 
-  int getCapacity() {
+  int getCapacity() const {
     return capacity;
   }
 
-  std::string getFile(int index) {
+  std::string getFile(int index) const {
     std::string result_file = "";
     if (index < capacity)
       result_file = files[index];
