89e992f58ceed68630205ce13cc7bafa653b0b4e
katheroine <katheroine@gmail.com>
2023-05-03 12:53:06 +0200

    C++ callback function [#functions]

---
+++ c++/functions/callbacks.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using std::string, std::cout, std::cin, std::endl;
+
+double sourceValue(string prompt, string (*validate)(double value))
+{
+  double value;
+  string validation_message = "";
+
+  do {
+    if (! validation_message.empty()) {
+      cout << validation_message << endl
+        << "Try again." << endl;
+    }
+    cout << prompt;
+    cin >> value;
+
+    validation_message = validate(value);
+  } while (! validation_message.empty());
+
+  return value;
+}
+
+string validateTemperatureInCelsius(double value)
+{
+  string message = "";
+
+  if (value > 26) {
+    message = "Temperature is to high for human health.";
+  } else if (value < 22) {
+    message = "Temperature is to low for human health.";
+  }
+
+  return message;
+}
+
+int main()
+{
+  double temperature;
+  temperature = sourceValue("Give the ambient temperature in degrees Celsius: ", validateTemperatureInCelsius);
+  cout << "Tempetature has been set to " << temperature << " degree Celsius." << endl;
+}
