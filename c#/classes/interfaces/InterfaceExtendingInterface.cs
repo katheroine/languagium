bebcddf76a6e15644aa75ccd5d649382f254deac
katheroine <katheroine@gmail.com>
2023-05-23 17:50:26 +0200

    C# interface extending another interface [#classes]

---
+++ c#/classes/interfaces/InterfaceExtendingInterface.cs
@@ -0,0 +1,46 @@
+interface Labelled {
+  string GetLabel();
+}
+
+interface Presentation : Labelled {
+  void Show();
+}
+
+abstract class Datum : Presentation {
+  protected string Description;
+
+  public Datum(string datumDescription) {
+    Description = datumDescription;
+  }
+
+  public string GetLabel() { // Must be implemented.
+    return ("Description: " + Description);
+  }
+
+  public void Show() { // Must be implemented.
+  }
+}
+
+class Content : Datum {
+  protected string Core;
+
+  public Content(string contentCore, string contentDescription) : base(contentDescription) {
+    Core = contentCore;
+  }
+
+  public new void Show() {
+    System.Console.WriteLine(GetLabel() + "\n"
+      + "Core: " + Core
+    );
+  }
+}
+
+class InterfaceExtendingInterface {
+  public static void Main(string[] args) {
+    Content lectio = new Content(
+      "In omnibus requiem quaesivi, et nusquam inveni nisi in angulo cum libro.",
+      "De beneficiis lectionis"
+    );
+    lectio.Show();
+  }
+}
