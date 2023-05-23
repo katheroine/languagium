9a3944ddf0a50f51b03b6a2fda67d779d20065b5
katheroine <katheroine@gmail.com>
2023-05-23 12:13:36 +0200

    Java class implementing multiple interfaces [#classes]

---
+++ java/classes/interfaces/ImplementingMultipleInterfaces.java
@@ -0,0 +1,48 @@
+interface Labelled {
+  public String getLabel();
+}
+
+interface Presentation {
+  public void show();
+}
+
+abstract class Datum implements Labelled, Presentation {
+  protected String description;
+
+  public Datum(String datumDescription) {
+    description = datumDescription;
+  }
+
+  public String getLabel() { // Can be implemented here or must be implemented in derived classes.
+    return ("Description: " + description);
+  }
+}
+
+class Content extends Datum {
+  protected String core;
+
+  public Content(String contentCore, String contentDescription) {
+    super(contentDescription);
+    core = contentCore;
+  }
+
+  public String formatCoreAsText() {
+    return ("Core: " + core);
+  }
+
+  public void show() { // Must be implemented.
+    System.out.println(getLabel() + "\n"
+      + "Core: " + core
+    );
+  }
+}
+
+class ImplementingMultipleInterfaces {
+  public static void main(String[] args) {
+    Content lectio = new Content(
+      "In omnibus requiem quaesivi, et nusquam inveni nisi in angulo cum libro.",
+      "De beneficiis lectionis"
+    );
+    lectio.show();
+  }
+}
