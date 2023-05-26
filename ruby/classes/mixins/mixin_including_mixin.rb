bf0db04096c253b846b83226ca4b095a232af18f
katheroine <katheroine@gmail.com>
2023-05-26 14:28:11 +0200

    Ruby mixin including mixin [#classes]

---
+++ ruby/classes/mixins/mixin_including_mixin.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby2.7
+
+module Describable
+  DESCRIPTION_TITLE = "Description: "
+
+  presentationTitle = ""
+end
+
+module Presentable
+  include Describable
+
+  CORE_TITLE = "Core: "
+
+  def show()
+    if (@presentationTitle.length)
+      print(@presentationTitle, "\n")
+    end
+    print(DESCRIPTION_TITLE, getLabel(), "\n",
+      CORE_TITLE, getCore(), "\n"
+    )
+  end
+end
+
+class Value
+  include Presentable
+
+  def initialize(name, value, presentationTitle = "")
+    @name = name
+    @value = value
+    @presentationTitle = presentationTitle
+  end
+
+  def getLabel()
+    @name
+  end
+
+  def getCore()
+    @value
+  end
+end
+
+class Content
+  include Presentable
+
+  def initialize(description, content, presentationTitle = "")
+    @description = description
+    @content = content
+    @presentationTitle = presentationTitle
+  end
+
+  def getLabel()
+    @description
+  end
+
+  def getCore()
+    @content
+  end
+end
+
+temp = Value.new(23.2, "Good ambient temperature", "My favourite temperature")
+temp.show()
+
+print("\n")
+
+lectio = Content.new(
+  "In omnibus requiem quaesivi, et nusquam inveni nisi in angulo cum libro.",
+  "De beneficiis lectionis",
+  "My favourite cite"
+)
+lectio.show()
+
+print("\n")
