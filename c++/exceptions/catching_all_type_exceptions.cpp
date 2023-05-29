a0f9458aa8f73abe7c9e1770f95430fc3b5b36c9
katheroine <katheroine@gmail.com>
2023-05-29 12:20:46 +0200

    C++ catching all-type exceptions [#exceptions]

---
+++ c++/exceptions/catching_all_type_exceptions.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using std::string, std::cout, std::cin, std::endl;
+
+class NumberValueException {
+public:
+  int number;
+  string message;
+};
+
+class ZeroException : public NumberValueException {
+public:
+  ZeroException(int number) {
+    this->number = number;
+    message = "0 number has beign given.";
+  }
+};
+
+class OneException : public NumberValueException {
+public:
+  OneException(int number) {
+    this->number = number;
+    message = "1 number has beign given.";
+  }
+};
+
+class ThousandException : public NumberValueException {
+public:
+  ThousandException(int number) {
+    this->number = number;
+    message = "1000 number has beign given.";
+  }
+};
+
+int drawNumber() {
+  cout << "Give some number: ";
+  int number;
+  cin >> number;
+
+  if (number == 0) {
+    ZeroException exception(number);
+    throw exception;
+  } else if (number == 1) {
+    OneException exception(number);
+    throw exception;
+  } else if (number == 1000) {
+    ThousandException exception(number);
+    throw exception;
+  } else if (number == 10000) { // Unhandled exception.
+    NumberValueException exception;
+    throw exception;
+  }
+
+  return number;
+}
+
+int main() {
+  cout << "Program begin..." << endl;
+
+  try {
+    cout << "Risky code..." << endl;
+
+    int number = drawNumber();
+
+    cout << "Given number didn't case exception throwing." << endl;
+  } catch (ZeroException e) {
+    cout << "CASE 1: " << e.message << " (" << e.number << ")" << endl;
+  } catch (OneException e) {
+    cout << "CASE 2: " << e.message << " (" << e.number << ")" << endl;
+  } catch (ThousandException e) {
+    cout << "CASE 3: " << e.message << " (" << e.number << ")" << endl;
+  } catch (...) {
+    cout << "Exception of unknown type catched. Rethrowing..." << endl;
+    throw;
+  }
+
+  cout << "Program end..." << endl;
+}
