b32960e018600e9e8d0ae9695c98d8155bbd5032
katheroine <katheroine@gmail.com>
2023-06-12 18:37:04 +0200

    Pascal exceptions hierarchy [#exceptions]

---
+++ pascal/exceptions/exceptions_hierarchy.pas
@@ -0,0 +1,85 @@
+{$MODE OBJFPC}
+
+program Exceptions;
+
+type
+  NumberValueException = class
+    public
+      number: integer;
+      message: string;
+
+    constructor create(exception_number: integer);
+  end;
+
+  ZeroException = class(NumberValueException)
+    constructor create(exception_number: integer);
+  end;
+
+  OneException = class(NumberValueException)
+    constructor create(exception_number: integer);
+  end;
+
+  ThousandException = class(NumberValueException)
+    constructor create(exception_number: integer);
+  end;
+
+constructor NumberValueException.create(exception_number: integer);
+begin
+  number := exception_number;
+  message := 'Some number has beign given.';
+end;
+
+constructor ZeroException.create(exception_number: integer);
+begin
+  number := exception_number;
+  message := '0 number has beign given.';
+end;
+
+constructor OneException.create(exception_number: integer);
+begin
+  number := exception_number;
+  message := '1 number has beign given.';
+end;
+
+constructor ThousandException.create(exception_number: integer);
+begin
+  number := exception_number;
+  message := '1000 number has beign given.';
+end;
+
+function drawNumber(): integer;
+var number: integer;
+begin
+  write('Give some number: ');
+  readln(number);
+
+  if (number = 0) then
+    raise ZeroException.create(number)
+  else if (number = 1) then
+    raise OneException.create(number)
+  else if (number = 10) then
+    raise NumberValueException.create(number)
+  else if (number = 1000) then
+    raise ThousandException.create(number);
+
+  drawNumber := number;
+end;
+
+var number: integer;
+
+begin
+  writeln('Program begin...');
+
+  try
+    number := drawNumber();
+
+    writeln('Given number ', number, ' didn''t case exception throwing.');
+  except
+    on e: ZeroException do writeln('CASE 1: ', e.message, ' (', e.number, ')');
+    on e: OneException do writeln('CASE 2: ', e.message, ' (', e.number, ')');
+    on e: NumberValueException do writeln('CASE 0: ', e.message, ' (', e.number, ')');
+    on e: ThousandException do writeln('CASE 3: ', e.message, ' (', e.number, ')'); // Will be never catched.
+  end;
+
+  writeln('Program end...');
+end.
