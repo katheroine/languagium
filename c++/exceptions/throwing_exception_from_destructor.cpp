4c5603a745269ad56c4f0102a2534082e3333509
katheroine <katheroine@gmail.com>
2023-07-12 12:16:31 +0200

    C++ throwing exception from destructor [#exceptions]

---
+++ c++/exceptions/throwing_exception_from_destructor.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using std::string, std::cout, std::cin, std::endl;
+
+class AnotherException
+{
+public:
+  AnotherException()
+  {
+    cout << "Creation of AnotherExcetion..." << endl;
+  }
+
+  ~AnotherException()
+  {
+    cout << "Destruction of AnotherExcetion..." << endl;
+  }
+};
+
+class SomeException
+{
+public:
+  SomeException()
+  {
+    cout << "Creation of SomeException..." << endl;
+  }
+
+  ~SomeException()
+  {
+    cout << "Destruction of SomeException..." << endl;
+  }
+};
+
+class Something
+{
+public:
+  Something()
+  {
+    cout << "Creation of Something..." << endl;
+  }
+
+  ~Something()
+  {
+    cout << "Destruction of Something..." << endl;
+
+    // THIS IS A BAD PRACTICE SO DO NOT DO IT.
+    AnotherException another_exception;
+    cout << "Throwing of AnotherException." << endl;
+    throw another_exception;
+  }
+};
+
+int main()
+{
+  cout << "THIS IS A BAD PRACTICE SO DO NOT DO IT." << endl << endl;
+
+  try
+  {
+    Something something;
+    SomeException some_exception;
+    cout << "Throwing of SomeException." << endl;
+    throw some_exception;
+  } catch (SomeException e) {
+    cout << "Catching of SomeException." << endl;
+  } catch (AnotherException e) {
+    cout << "Catching of AnotherException." << endl;
+  }
+}
