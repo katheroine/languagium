0b7b3215f30e9fbc71cc4462e25d39fd58f288c6
katheroine <katheroine@gmail.com>
2024-08-25 19:25:54 +0200

    C++ variables [#variables]

---
+++ c++/literals_constants_variables/variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+  string name;
+  int quantity;
+
+  name = "C++";
+  quantity = 5;
+
+  cout << "I am the " << name
+    << " programmer with " << quantity
+    << " years of experience." << endl;
+}
