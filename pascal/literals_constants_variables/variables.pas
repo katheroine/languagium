5ba3f7e397c00d9a28eff23a73f004b161d80c53
katheroine <katheroine@gmail.com>
2024-08-25 16:06:16 +0200

    Pascal variables [#variables]

---
+++ pascal/literals_constants_variables/variables.pas
@@ -0,0 +1,16 @@
+program Variables;
+
+var
+  name: string;
+  quantity: integer;
+
+begin
+  name := 'Pascal';
+  quantity := 5;
+
+  writeln(
+    'I am the ', name,
+    ' programmer with ', quantity,
+    ' years of experience.'
+  );
+end.
